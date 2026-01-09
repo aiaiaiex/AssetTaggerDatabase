752895926738f0bfb43f95eaf30299cc7c27e539
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-09 16:34:29 +0800

    Rename unique index

--- AssetTaggerDatabaseProject/dbo/Tables/EndUser.sql
+++ AssetTaggerDatabaseProject/dbo/Tables/EndUser.sql
@@ -1,19 +1,12 @@
 CREATE TABLE [dbo].[EndUser] (
-    [EndUserID]           UNIQUEIDENTIFIER NOT NULL,
+    [EndUserID]           UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_EndUser_EndUserID] DEFAULT (newid()) NOT NULL,
     [EndUserName]         NVARCHAR (50)    NOT NULL,
-    [EndUserPasswordHash] NCHAR (32)       NOT NULL
+    [EndUserPasswordHash] NCHAR (32)       NOT NULL,
+    CONSTRAINT [PK_EndUser] PRIMARY KEY CLUSTERED ([EndUserID] ASC)
 );
 GO
 
-CREATE UNIQUE NONCLUSTERED INDEX [Index_EndUser_1]
+CREATE UNIQUE NONCLUSTERED INDEX [UX_EndUser_EndUserName]
     ON [dbo].[EndUser]([EndUserName] ASC);
-GO
-
-ALTER TABLE [dbo].[EndUser]
-    ADD CONSTRAINT [PK_EndUser] PRIMARY KEY CLUSTERED ([EndUserID] ASC);
-GO
-
-ALTER TABLE [dbo].[EndUser]
-    ADD CONSTRAINT [DEFAULT_EndUser_EndUserID] DEFAULT (newid()) FOR [EndUserID];
 GO
 
