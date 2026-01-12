9760d70ef7dcfb696a2de8f8cb9211a85116005f
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-12 17:30:46 +0800

    Shorten AssetTransferReceivingCompanyID to ReceivingCompanyID

--- dbo/Tables/AssetTransfer.sql
+++ dbo/Tables/AssetTransfer.sql
@@ -1,13 +1,13 @@
 CREATE TABLE [dbo].[AssetTransfer] (
-    [AssetTransferID]                 UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_AssetTransfer_AssetTransferID] DEFAULT (newid()) NOT NULL,
-    [AssetTransferDate]               DATETIME         NOT NULL,
-    [AssetTransferPrice]              MONEY            NULL,
-    [AssetID]                         UNIQUEIDENTIFIER NOT NULL,
-    [CompanyID]                       UNIQUEIDENTIFIER NOT NULL,
-    [AssetTransferReceivingCompanyID] UNIQUEIDENTIFIER NOT NULL,
+    [AssetTransferID]    UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_AssetTransfer_AssetTransferID] DEFAULT (newid()) NOT NULL,
+    [AssetTransferDate]  DATETIME         NOT NULL,
+    [AssetTransferPrice] MONEY            NULL,
+    [AssetID]            UNIQUEIDENTIFIER NOT NULL,
+    [CompanyID]          UNIQUEIDENTIFIER NOT NULL,
+    [ReceivingCompanyID] UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_AssetTransfer] PRIMARY KEY CLUSTERED ([AssetTransferID] ASC),
     CONSTRAINT [FK_AssetTransfer_Asset] FOREIGN KEY ([AssetID]) REFERENCES [dbo].[Asset] ([AssetID]),
-    CONSTRAINT [FK_AssetTransfer_Company_AssetTransferReceivingCompanyID] FOREIGN KEY ([AssetTransferReceivingCompanyID]) REFERENCES [dbo].[Company] ([CompanyID]),
-    CONSTRAINT [FK_AssetTransfer_Company_CompanyID] FOREIGN KEY ([CompanyID]) REFERENCES [dbo].[Company] ([CompanyID])
+    CONSTRAINT [FK_AssetTransfer_Company_CompanyID] FOREIGN KEY ([CompanyID]) REFERENCES [dbo].[Company] ([CompanyID]),
+    CONSTRAINT [FK_AssetTransfer_Company_ReceivingCompanyID] FOREIGN KEY ([ReceivingCompanyID]) REFERENCES [dbo].[Company] ([CompanyID])
 );
 
