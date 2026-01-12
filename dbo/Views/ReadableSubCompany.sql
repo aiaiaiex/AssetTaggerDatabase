4acad7ee0387c429d66dce43fabb6203d8ce1133
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-12 17:31:51 +0800

    Create view for SubCompany to make it human-readable

---
+++ dbo/Views/ReadableSubCompany.sql
@@ -0,0 +1,10 @@
+CREATE VIEW [dbo].[ReadableSubCompany]
+AS
+  SELECT sc.ParentCompanyID, co.CompanyName as ParentCompanyName, sc.CompanyID, com.CompanyName
+  FROM [dbo].[SubCompany] sc
+  INNER JOIN [dbo].[Company] co
+    ON co.CompanyID = sc.ParentCompanyID
+  INNER JOIN [dbo].[Company] com
+    ON com.CompanyID = sc.CompanyID
+GO
+
