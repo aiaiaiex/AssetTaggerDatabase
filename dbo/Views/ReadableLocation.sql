27ead57fb9f7709eefd737ab88c20080b9d3815c
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-12 17:32:28 +0800

    Create view for Location to make it human-readable

---
+++ dbo/Views/ReadableLocation.sql
@@ -0,0 +1,8 @@
+CREATE VIEW [dbo].[ReadableLocation]
+AS
+  SELECT l.LocationID, l.LocationAddress, l.BuildingID, b.BuilidingName
+  FROM [dbo].[Location] l
+  INNER JOIN [dbo].[Building] b
+    ON b.BuilidingID = l.BuildingID
+GO
+
