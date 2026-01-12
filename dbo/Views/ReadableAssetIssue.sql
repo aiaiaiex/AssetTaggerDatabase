9295345733a38b5b6984314590cf708dde071f1f
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-12 17:32:54 +0800

    Create view for AssetIssue to make it human-readable

---
+++ dbo/Views/ReadableAssetIssue.sql
@@ -0,0 +1,12 @@
+CREATE VIEW [dbo].[ReadableAssetIssue]
+AS
+  SELECT ai.AssetIssueID, ai.AssetIssueTitle, ai.AssetIssueDesc, ai.AssetIssueDate, ai.AssetID, p.ProductName, ai.EmployeeID, e.EmployeeFullName
+  FROM [dbo].[AssetIssue] ai
+  INNER JOIN [dbo].[Asset] a
+    ON a.AssetID = ai.AssetID
+  INNER JOIN [dbo].[Product] p
+    ON p.ProductID = a.ProductID
+  INNER JOIN [dbo].[Employee] e
+    ON e.EmployeeID = ai.EmployeeID
+GO
+
