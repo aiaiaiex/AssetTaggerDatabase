12ad54cdba4fbfec25096d974e07767e26a29bc4
Guiliani <guilianipobre11@gmail.com>
2026-01-12 10:55:57 +0800

    Remove ALTER TABLE on queries of AssetFix and AssetIssue

--- AssetTaggerDatabaseProject/dbo/Tables/AssetIssue.sql
+++ AssetTaggerDatabaseProject/dbo/Tables/AssetIssue.sql
@@ -1,26 +1,12 @@
 CREATE TABLE [dbo].[AssetIssue] (
-    [AssetIssueID]    UNIQUEIDENTIFIER NOT NULL,
+    [AssetIssueID]    UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_AssetIssues_AssetIssueID] DEFAULT (newid()) NOT NULL,
     [AssetIssueTitle] NVARCHAR (50)    NOT NULL,
     [AssetIssueDesc]  NTEXT            NULL,
     [AssetIssueDate]  DATETIME         NULL,
     [AssetID]         UNIQUEIDENTIFIER NOT NULL,
-    [EmployeeID]      UNIQUEIDENTIFIER NOT NULL
+    [EmployeeID]      UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_AssetIssues] PRIMARY KEY CLUSTERED ([AssetIssueID] ASC),
+    CONSTRAINT [FK_AssetIssues_Asset] FOREIGN KEY ([AssetID]) REFERENCES [dbo].[Asset] ([AssetID]),
+    CONSTRAINT [FK_AssetIssues_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID])
 );
-GO
-
-ALTER TABLE [dbo].[AssetIssue]
-    ADD CONSTRAINT [FK_AssetIssues_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID]);
-GO
-
-ALTER TABLE [dbo].[AssetIssue]
-    ADD CONSTRAINT [FK_AssetIssues_Asset] FOREIGN KEY ([AssetID]) REFERENCES [dbo].[Asset] ([AssetID]);
-GO
-
-ALTER TABLE [dbo].[AssetIssue]
-    ADD CONSTRAINT [DEFAULT_AssetIssues_AssetIssueID] DEFAULT (newid()) FOR [AssetIssueID];
-GO
-
-ALTER TABLE [dbo].[AssetIssue]
-    ADD CONSTRAINT [PK_AssetIssues] PRIMARY KEY CLUSTERED ([AssetIssueID] ASC);
-GO
 
