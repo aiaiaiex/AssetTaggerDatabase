2b6557b66bbdb6037a693c09f476c23543f77e92
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-12 08:28:01 +0800

    Remove ALTER TABLE commands

--- AssetTaggerDatabaseProject/dbo/Tables/Product.sql
+++ AssetTaggerDatabaseProject/dbo/Tables/Product.sql
@@ -1,21 +1,10 @@
 CREATE TABLE [dbo].[Product] (
-    [ProductID]           UNIQUEIDENTIFIER NOT NULL,
+    [ProductID]           UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_Product_ProductID] DEFAULT (newid()) NOT NULL,
     [ProductName]         NVARCHAR (50)    NULL,
     [ProductModelNumber]  NVARCHAR (50)    NULL,
     [ProductManufacturer] NVARCHAR (50)    NULL,
-    [CategoryID]          UNIQUEIDENTIFIER NOT NULL
+    [CategoryID]          UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([ProductID] ASC),
+    CONSTRAINT [FK_Product_Category] FOREIGN KEY ([CategoryID]) REFERENCES [dbo].[Category] ([CategoryID])
 );
-GO
-
-ALTER TABLE [dbo].[Product]
-    ADD CONSTRAINT [DEFAULT_Product_ProductID] DEFAULT (newid()) FOR [ProductID];
-GO
-
-ALTER TABLE [dbo].[Product]
-    ADD CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([ProductID] ASC);
-GO
-
-ALTER TABLE [dbo].[Product]
-    ADD CONSTRAINT [FK_Product_Category] FOREIGN KEY ([CategoryID]) REFERENCES [dbo].[Category] ([CategoryID]);
-GO
 
