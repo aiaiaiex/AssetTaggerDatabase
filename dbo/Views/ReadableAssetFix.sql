5b99856f268e43749ffa7e5a2d28094fa8536e21
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-12 17:33:18 +0800

    Create view for AssetFix to make it human-readable

---
+++ dbo/Views/ReadableAssetFix.sql
@@ -0,0 +1,14 @@
+CREATE VIEW [dbo].[ReadableAssetFix]
+AS
+  SELECT af.AssetFixID, af.AssetIssueID, ai.AssetIssueTitle, p.ProductName, af.AssetFixDateStart, af.AssetFixCost, af.AssetFixDateEnd, af.AssetFixTitle, af.AssetFixDesc, af.AssetFixed, af.EmployeeID, e.EmployeeFullName
+  FROM [dbo].[AssetFix] af
+  INNER JOIN [dbo].[AssetIssue] ai
+    ON ai.AssetIssueID = af.AssetIssueID
+  INNER JOIN [dbo].[Asset] a
+    ON a.AssetID = ai.AssetID
+  INNER JOIN [dbo].[Product] p
+    ON p.ProductID = a.ProductID
+  INNER JOIN [dbo].[Employee] e
+    ON e.EmployeeID = af.EmployeeID
+GO
+
