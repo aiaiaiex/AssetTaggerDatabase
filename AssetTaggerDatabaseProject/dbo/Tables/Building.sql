9603fbc9a9935e0b4fdbbf30e7b3520c4b087cc0
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-12 10:29:08 +0800

    Make CompanyID and SubCompanyID nullable

--- AssetTaggerDatabaseProject/dbo/Tables/Building.sql
+++ AssetTaggerDatabaseProject/dbo/Tables/Building.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[Building] (
     [BuilidingID]     UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_Building_BuilidingID] DEFAULT (newid()) NOT NULL,
     [BuilidingName]   NVARCHAR (50)    NOT NULL,
-    [CompanyID]       UNIQUEIDENTIFIER NOT NULL,
-    [SubCompanyID]    UNIQUEIDENTIFIER NOT NULL,
+    [CompanyID]       UNIQUEIDENTIFIER NULL,
+    [SubCompanyID]    UNIQUEIDENTIFIER NULL,
     [BuildingAddress] NVARCHAR (50)    NOT NULL,
     CONSTRAINT [PK_Building] PRIMARY KEY CLUSTERED ([BuilidingID] ASC),
     CONSTRAINT [CK_SubORCompanyIDNotNull] CHECK ([CompanyID] IS NOT NULL OR [SubCompanyID] IS NOT NULL),
@@ -12,11 +12,11 @@ CREATE TABLE [dbo].[Building] (
 
 
 GO
-CREATE UNIQUE NONCLUSTERED INDEX [UX_Building_BuildingAddress]
-    ON [dbo].[Building]([BuildingAddress] ASC);
+CREATE UNIQUE NONCLUSTERED INDEX [UX_Building_BuildingName]
+    ON [dbo].[Building]([BuilidingName] ASC);
 
 
 GO
-CREATE UNIQUE NONCLUSTERED INDEX [UX_Building_BuildingName]
-    ON [dbo].[Building]([BuilidingName] ASC);
+CREATE UNIQUE NONCLUSTERED INDEX [UX_Building_BuildingAddress]
+    ON [dbo].[Building]([BuildingAddress] ASC);
 
