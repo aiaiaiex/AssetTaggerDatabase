a2ebe7d1f3fad389e8af2413731a58098093eb4a
Guiliani <guilianipobre11@gmail.com>
2026-01-12 10:55:57 +0800

    Remove ALTER TABLE on queries of AssetFix and AssetIssue

--- AssetTaggerDatabaseProject/dbo/Tables/AssetFix.sql
+++ AssetTaggerDatabaseProject/dbo/Tables/AssetFix.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[AssetFix] (
-    [AssetFixID]        UNIQUEIDENTIFIER NOT NULL,
+    [AssetFixID]        UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_AssetFIx_AssetFixID] DEFAULT (newid()) NOT NULL,
     [AssetIssueID]      UNIQUEIDENTIFIER NOT NULL,
     [AssetFixDateStart] DATETIME         NULL,
     [AssetFixCost]      MONEY            NULL,
@@ -7,23 +7,9 @@ CREATE TABLE [dbo].[AssetFix] (
     [AssetFixTitle]     NVARCHAR (50)    NOT NULL,
     [AssetFixDesc]      NTEXT            NULL,
     [AssetFixed]        BIT              NOT NULL,
-    [EmployeeID]        UNIQUEIDENTIFIER NOT NULL
+    [EmployeeID]        UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_AssetFIx] PRIMARY KEY CLUSTERED ([AssetFixID] ASC),
+    CONSTRAINT [FK_AssetFix_AssetIssue] FOREIGN KEY ([AssetIssueID]) REFERENCES [dbo].[AssetIssue] ([AssetIssueID]),
+    CONSTRAINT [FK_AssetFIx_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID])
 );
-GO
-
-ALTER TABLE [dbo].[AssetFix]
-    ADD CONSTRAINT [DEFAULT_AssetFIx_AssetFixID] DEFAULT (newid()) FOR [AssetFixID];
-GO
-
-ALTER TABLE [dbo].[AssetFix]
-    ADD CONSTRAINT [PK_AssetFIx] PRIMARY KEY CLUSTERED ([AssetFixID] ASC);
-GO
-
-ALTER TABLE [dbo].[AssetFix]
-    ADD CONSTRAINT [FK_AssetFIx_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID]);
-GO
-
-ALTER TABLE [dbo].[AssetFix]
-    ADD CONSTRAINT [FK_AssetFix_AssetIssue] FOREIGN KEY ([AssetIssueID]) REFERENCES [dbo].[AssetIssue] ([AssetIssueID]);
-GO
 
