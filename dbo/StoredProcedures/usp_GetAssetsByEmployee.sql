21eea827a220be16a67b3a89cec71192478bf5bb
Guiliani <guilianipobre11@gmail.com>
2026-01-13 15:11:36 +0800

    Create usp_GeatAssetsByEmployees procedure

---
+++ dbo/StoredProcedures/usp_GetAssetsByEmployee.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE [dbo].[usp_GetAssetsByEmployee]
+    @EmployeeID UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        AssetID
+    FROM [dbo].[Asset] 
+    WHERE EmployeeID = @EmployeeID;
+END
+GO
+
