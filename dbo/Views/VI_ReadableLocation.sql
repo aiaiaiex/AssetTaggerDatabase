77d81f3d992882beba1e900ab3345fe500018f56
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-13 15:58:41 +0800

    Fix typo of "Builiding" to "Building"

--- dbo/Views/VI_ReadableLocation.sql
+++ dbo/Views/VI_ReadableLocation.sql
@@ -1,8 +1,8 @@
 CREATE VIEW [dbo].[VI_ReadableLocation]
 AS
-  SELECT l.LocationID, l.LocationAddress, l.BuildingID, b.BuilidingName
+  SELECT l.LocationID, l.LocationAddress, l.BuildingID, b.BuildingName
   FROM [dbo].[Location] l
   INNER JOIN [dbo].[Building] b
-    ON b.BuilidingID = l.BuildingID
+    ON b.BuildingID = l.BuildingID
 GO
 
