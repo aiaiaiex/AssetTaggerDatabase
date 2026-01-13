a53aff71112593d45d70ce6822b6b46b788dcbb9
Guiliani <guilianipobre11@gmail.com>
2026-01-13 15:22:05 +0800

    Fixed spelling error for Building

--- dbo/Tables/Building.sql
+++ dbo/Tables/Building.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [dbo].[Building] (
-    [BuilidingID]     UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_Building_BuilidingID] DEFAULT (newid()) NOT NULL,
-    [BuilidingName]   NVARCHAR (50)    NOT NULL,
+    [BuildingID]      UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_Building_BuilidingID] DEFAULT (newid()) NOT NULL,
+    [BuildingName]    NVARCHAR (50)    NOT NULL,
     [CompanyID]       UNIQUEIDENTIFIER NOT NULL,
     [BuildingAddress] NVARCHAR (50)    NOT NULL,
-    CONSTRAINT [PK_Building] PRIMARY KEY CLUSTERED ([BuilidingID] ASC),
+    CONSTRAINT [PK_Building] PRIMARY KEY CLUSTERED ([BuildingID] ASC),
     CONSTRAINT [FK_Building_Company] FOREIGN KEY ([CompanyID]) REFERENCES [dbo].[Company] ([CompanyID])
 );
 
@@ -15,5 +15,5 @@ CREATE UNIQUE NONCLUSTERED INDEX [UX_Building_BuildingAddress]
 
 GO
 CREATE UNIQUE NONCLUSTERED INDEX [UX_Building_BuildingName]
-    ON [dbo].[Building]([BuilidingName] ASC);
+    ON [dbo].[Building]([BuildingName] ASC);
 
