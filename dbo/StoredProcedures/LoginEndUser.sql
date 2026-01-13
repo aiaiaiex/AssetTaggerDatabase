13e813c8c88823141f3b03bcc2a1414b133f4d22
Guiliani <guilianipobre11@gmail.com>
2026-01-13 10:20:50 +0800

    Create LoginEndUser procedure

---
+++ dbo/StoredProcedures/LoginEndUser.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE LoginEndUser
+    @EndUserName NVARCHAR(50),
+    @EndUserPassword NVARCHAR(255)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT EndUserID
+    FROM [dbo].[EndUser]
+    WHERE EndUserName = @EndUserName 
+    AND EndUserPasswordHash = CONVERT(NCHAR(32), HASHBYTES('SHA2_256', @EndUserPassword));
+END
+GO
+
