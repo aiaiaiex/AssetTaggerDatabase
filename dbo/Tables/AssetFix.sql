b3927f222db3b8a1f28041320f9c9d8dac763edf
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-14 08:23:34 +0800

    Constrain AssetFixCost to be non-negative

--- dbo/Tables/AssetFix.sql
+++ dbo/Tables/AssetFix.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[AssetFix] (
-    [AssetFixID]        UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_AssetFix_AssetFixID] DEFAULT (newid()) NOT NULL,
+    [AssetFixID]        UNIQUEIDENTIFIER CONSTRAINT [DEFAULT_AssetFIx_AssetFixID] DEFAULT (newid()) NOT NULL,
     [AssetIssueID]      UNIQUEIDENTIFIER NOT NULL,
     [AssetFixDateStart] DATETIME         NULL,
     [AssetFixCost]      MONEY            NULL,
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[AssetFix] (
     [AssetFixDesc]      NTEXT            NULL,
     [AssetFixed]        BIT              NOT NULL,
     [EmployeeID]        UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT [PK_AssetFix] PRIMARY KEY CLUSTERED ([AssetFixID] ASC),
+    CONSTRAINT [PK_AssetFIx] PRIMARY KEY CLUSTERED ([AssetFixID] ASC),
+    CONSTRAINT [CK_AssetFix_AssetFixCost] CHECK ([AssetFixCost]>=(0)),
     CONSTRAINT [FK_AssetFix_AssetIssue] FOREIGN KEY ([AssetIssueID]) REFERENCES [dbo].[AssetIssue] ([AssetIssueID]),
     CONSTRAINT [FK_AssetFix_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID])
 );
