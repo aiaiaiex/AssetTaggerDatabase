8f3cae1e75bd0476c6555e76c07dfb350fab30c3
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-14 14:01:07 +0800

    Create usp_GetSubsidiariesOfCompany stored procedure

---
+++ dbo/StoredProcedures/usp_GetSubsidiariesOfCompany.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE [dbo].[usp_GetSubsidiariesOfCompany]
+    @ParentCompanyID UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT CompanyID
+    FROM [dbo].[Company]
+    WHERE ParentCompanyID = @ParentCompanyID;
+END
+GO
