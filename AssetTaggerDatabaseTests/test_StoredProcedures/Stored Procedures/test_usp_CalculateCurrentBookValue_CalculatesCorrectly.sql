598fed51f66778d43070d18697cbb6c2e95d42d3
Guiliani <guilianipobre11@gmail.com>
2026-01-19 14:51:54 +0800

    Create test_usp_CalculateCurrentBookValue_CalculatesCorrectly procedure

---
+++ AssetTaggerDatabaseTests/test_StoredProcedures/Stored Procedures/test_usp_CalculateCurrentBookValue_CalculatesCorrectly.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [test_StoredProcedures].[test_usp_CalculateCurrentBookValue_CalculatesCorrectly]
+AS
+BEGIN
+
+    EXEC tSQLt.FakeTable '[dbo].[Asset]';
+
+    DECLARE @AssetID UNIQUEIDENTIFIER = NEWID();
+
+    DECLARE @TwoYearsAgo DATETIME = DATEADD(yy, -2, GETDATE());
+
+    INSERT INTO [dbo].[Asset] (AssetID, AssetPurchasePrice, AssetSalvageValue, AssetUsefulLife, AssetPurchaseDate)
+    VALUES (@AssetID, 10000, 1000, 9, @TwoYearsAgo);
+
+    CREATE TABLE #actual (CurrentBookValue MONEY);
+    
+    INSERT INTO #actual (CurrentBookValue)
+    EXEC [dbo].[usp_CalculateCurrentBookValue] @AssetID;
+
+    CREATE TABLE #expected (CurrentBookValue MONEY);
+    INSERT INTO #expected VALUES (8000);
+
+    EXEC tSQLt.AssertEqualsTable '#expected', '#actual';
+END;
