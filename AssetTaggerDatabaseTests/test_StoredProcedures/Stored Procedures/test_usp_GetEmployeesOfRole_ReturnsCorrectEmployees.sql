e7053f0c0591e0c03328ba56f8933ee0de238333
Guiliani <guilianipobre11@gmail.com>
2026-01-20 10:00:38 +0800

    Create test_usp_GetEmployeesOfRole_ReturnsCorrectEmployees test procedure

---
+++ AssetTaggerDatabaseTests/test_StoredProcedures/Stored Procedures/test_usp_GetEmployeesOfRole_ReturnsCorrectEmployees.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [test_StoredProcedures].[test_usp_GetEmployeesOfRole_ReturnsCorrectEmployees]
+AS
+BEGIN
+
+    EXEC tSQLt.FakeTable '[dbo].[Employee]';
+
+    DECLARE @TargetRoleID UNIQUEIDENTIFIER = NEWID();
+    DECLARE @OtherRoleID  UNIQUEIDENTIFIER = NEWID();
+
+    DECLARE @Emp1 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @Emp2 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @NoiseEmp UNIQUEIDENTIFIER = NEWID();
+
+    INSERT INTO [dbo].[Employee] (EmployeeID, RoleID)
+    VALUES 
+        (@Emp1, @TargetRoleID),
+        (@Emp2, @TargetRoleID),
+        (@NoiseEmp, @OtherRoleID);
+
+    CREATE TABLE #actual (EmployeeID UNIQUEIDENTIFIER);
+
+    INSERT INTO #actual (EmployeeID)
+    EXEC [dbo].[usp_GetEmployeesOfRole] @RoleID = @TargetRoleID;
+
+    CREATE TABLE #expected (EmployeeID UNIQUEIDENTIFIER);
+    INSERT INTO #expected (EmployeeID) VALUES (@Emp1), (@Emp2);
+
+    EXEC tSQLt.AssertEqualsTable '#expected', '#actual';
+END;
