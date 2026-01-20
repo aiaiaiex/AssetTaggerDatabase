f16969632aeff44c8e61aec30c16ba95ac3a065b
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-20 11:33:57 +0800

    Create tSQL test class for table-valued functions

---
+++ AssetTaggerDatabaseTests/Security/test_TableValuedFunctions.sql
@@ -0,0 +1,7 @@
+﻿CREATE SCHEMA [test_TableValuedFunctions]
+    AUTHORIZATION [dbo];
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'tSQLt.TestClass', @value = 1, @level0type = N'SCHEMA', @level0name = N'test_TableValuedFunctions';
+
