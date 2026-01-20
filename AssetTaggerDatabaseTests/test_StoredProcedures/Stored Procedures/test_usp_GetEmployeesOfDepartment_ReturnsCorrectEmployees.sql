069ef006af8c1be416e21a910063752ac4e80419
Guiliani <guilianipobre11@gmail.com>
2026-01-20 09:51:05 +0800

    Create test_usp_GetEmployeesOfDepartment_ReturnsCorrectEmployees test procedure

---
+++ AssetTaggerDatabaseTests/test_StoredProcedures/Stored Procedures/test_usp_GetEmployeesOfDepartment_ReturnsCorrectEmployees.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [test_StoredProcedures].[test_usp_GetEmployeesOfDepartment_ReturnsCorrectEmployees]
+AS
+BEGIN
+
+    EXEC tSQLt.FakeTable '[dbo].[Employee]';
+
+    DECLARE @TargetDeptID UNIQUEIDENTIFIER = NEWID();
+    DECLARE @OtherDeptID  UNIQUEIDENTIFIER = NEWID();
+
+    DECLARE @Emp1 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @Emp2 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @NoiseEmp UNIQUEIDENTIFIER = NEWID();
+
+    INSERT INTO [dbo].[Employee] (EmployeeID, DepartmentID)
+    VALUES 
+        (@Emp1, @TargetDeptID),
+        (@Emp2, @TargetDeptID),
+        (@NoiseEmp, @OtherDeptID);
+
+    CREATE TABLE #actual (EmployeeID UNIQUEIDENTIFIER);
+
+    INSERT INTO #actual (EmployeeID)
+    EXEC [dbo].[usp_GetEmployeesOfDepartment] @DepartmentID = @TargetDeptID;
+
+    CREATE TABLE #expected (EmployeeID UNIQUEIDENTIFIER);
+    INSERT INTO #expected (EmployeeID) VALUES (@Emp1), (@Emp2);
+
+    EXEC tSQLt.AssertEqualsTable '#expected', '#actual';
+END;
