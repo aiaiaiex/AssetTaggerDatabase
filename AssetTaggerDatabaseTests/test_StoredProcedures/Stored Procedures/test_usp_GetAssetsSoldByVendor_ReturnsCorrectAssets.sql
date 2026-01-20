36537bafd5231c09dba68395507ee71dac4ac0b0
Guiliani <guilianipobre11@gmail.com>
2026-01-20 09:50:43 +0800

    Create test_usp_GetAssetsSoldByVendor_ReturnsCorrectAssets test procedure

---
+++ AssetTaggerDatabaseTests/test_StoredProcedures/Stored Procedures/test_usp_GetAssetsSoldByVendor_ReturnsCorrectAssets.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [test_StoredProcedures].[test_usp_GetAssetsSoldByVendor_ReturnsCorrectAssets]
+AS
+BEGIN
+
+    EXEC tSQLt.FakeTable '[dbo].[Asset]';
+
+    DECLARE @TargetVendorID UNIQUEIDENTIFIER = NEWID();
+    DECLARE @OtherVendorID  UNIQUEIDENTIFIER = NEWID();
+    
+    DECLARE @Asset1 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @Asset2 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @NoiseAsset UNIQUEIDENTIFIER = NEWID();
+
+    INSERT INTO [dbo].[Asset] (AssetID, VendorID)
+    VALUES 
+        (@Asset1, @TargetVendorID),
+        (@Asset2, @TargetVendorID),
+        (@NoiseAsset, @OtherVendorID);
+
+    CREATE TABLE #actual (AssetID UNIQUEIDENTIFIER);
+    
+    INSERT INTO #actual (AssetID)
+    EXEC [dbo].[usp_GetAssetsSoldByVendor] @VendorID = @TargetVendorID;
+
+    CREATE TABLE #expected (AssetID UNIQUEIDENTIFIER);
+    INSERT INTO #expected (AssetID) VALUES (@Asset1), (@Asset2);
+
+    EXEC tSQLt.AssertEqualsTable '#expected', '#actual';
+END;
