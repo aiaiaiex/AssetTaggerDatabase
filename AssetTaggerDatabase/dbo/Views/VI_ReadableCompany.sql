4be999ef9f20c061a2cdc692009529bd6b035271
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-21 17:44:20 +0800

    Change inner join to left join to include companies without a parent company

--- AssetTaggerDatabase/dbo/Views/VI_ReadableCompany.sql
+++ AssetTaggerDatabase/dbo/Views/VI_ReadableCompany.sql
@@ -3,5 +3,5 @@
 AS
   SELECT c.CompanyID, c.ParentCompanyID, co.CompanyName AS ParentCompanyName, c.CompanyName, c.CompanyAddress, c.CompanyCode
   FROM [dbo].[Company] c
-  INNER JOIN [dbo].[Company] co
+  LEFT JOIN [dbo].[Company] co
     ON c.ParentCompanyID = co.CompanyID
