ba2f8346731907ef4e0aa12898b169134311fc61
Guiliani <guilianipobre11@gmail.com>
2026-01-23 16:50:31 +0800

    Create test_DF_Employee_EmployeeID constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Employee_EmployeeID.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Employee_EmployeeID]
+AS
+BEGIN
+    -- Create dummy data for AssetIssue.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Employee]', @Defaults=1;
+
+    DECLARE @EmployeeFullName NVARCHAR(50) = 'Guil Pobre';
+
+    INSERT INTO [dbo].[Employee] (EmployeeFullName) VALUES
+    (@EmployeeFullName);
+
+    -- Actual ouput.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = EmployeeID from [dbo].[Employee];
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
