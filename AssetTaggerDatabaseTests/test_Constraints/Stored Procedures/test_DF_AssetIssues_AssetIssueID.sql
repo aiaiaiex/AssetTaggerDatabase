76ba042a11b95cef3130de6e17ee6bf77fa578c4
Guiliani <guilianipobre11@gmail.com>
2026-01-23 16:00:27 +0800

    Create test_DF_AssetIssues_AssetIssueID constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_AssetIssues_AssetIssueID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_AssetIssues_AssetIssueID]
+AS
+BEGIN
+    -- Create dummy data for AssetIssue.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[AssetIssue]', @Defaults=1;
+
+    DECLARE @AssetIssueTitle NVARCHAR(50) = 'Hard Drive Replacement';
+
+    INSERT INTO [dbo].[AssetIssue] (AssetIssueTitle) VALUES
+    (@AssetIssueTitle);
+
+    -- Actual ouput.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = AssetIssueID from [dbo].[AssetIssue];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
