7123ec66c377edca50e0fa01570e11509940f5a3
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-23 15:38:06 +0800

    Create test case for DF_AssetFix_AssetFixID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_AssetFix_AssetFixID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_AssetFix_AssetFixID]
+AS
+BEGIN
+    -- Create dummy data for AssetFix.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[AssetFix]', @Defaults=1;
+
+    DECLARE @AssetFixDateStart DATETIME = '2000-01-01';
+
+    INSERT INTO [dbo].[AssetFix] (AssetFixDateStart) VALUES
+    (@AssetFixDateStart);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = AssetFixID from [dbo].[AssetFix];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
