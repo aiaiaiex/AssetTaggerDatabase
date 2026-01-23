0e7bd40c3ff39c31c63078fc0221f48b6e57bc99
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-23 16:51:58 +0800

    Create test case for DF_Location_LocationID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Location_LocationID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Location_LocationID]
+AS
+BEGIN
+    -- Create dummy data for Location.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Location]', @Defaults=1;
+
+    DECLARE @LocationAddress NVARCHAR(50) = 'Location Address 01';
+
+    INSERT INTO [dbo].[Location] (LocationAddress) VALUES
+    (@LocationAddress);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = LocationID from [dbo].[Location];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
