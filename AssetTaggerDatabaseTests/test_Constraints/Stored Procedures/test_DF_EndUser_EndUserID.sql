a24e8431cb8a66a96798e21e142d3ca1a7ad1ada
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-23 16:47:07 +0800

    Create test case for DF_EndUser_EndUserID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_EndUser_EndUserID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_EndUser_EndUserID]
+AS
+BEGIN
+    -- Create dummy data for EndUser.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[EndUser]', @Defaults=1;
+
+    DECLARE @EndUserName NVARCHAR(50) = 'End User Name 01';
+
+    INSERT INTO [dbo].[EndUser] (EndUserName) VALUES
+    (@EndUserName);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = EndUserID from [dbo].[EndUser];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
