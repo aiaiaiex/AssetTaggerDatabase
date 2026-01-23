d5b79f45050fe41899d3178102cfa0b3b60f8c28
Guiliani <guilianipobre11@gmail.com>
2026-01-23 16:56:21 +0800

    Create test_DF_Product_ProductID constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Product_ProductID.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Product_ProductID]
+AS
+BEGIN
+    -- Create dummy data for AssetIssue.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Product]', @Defaults=1;
+
+    DECLARE @ProductManufacturer NVARCHAR(50) = 'Nvidia';
+
+    INSERT INTO [dbo].[Product] (ProductManufacturer) VALUES
+    (@ProductManufacturer);
+
+    -- Actual ouput.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = ProductID from [dbo].[Product];
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
