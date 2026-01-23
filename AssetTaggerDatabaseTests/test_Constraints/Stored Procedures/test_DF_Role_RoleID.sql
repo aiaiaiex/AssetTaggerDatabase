4a16a16341787c7880a5c6157d6ce1d46c22a3cc
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-23 16:58:01 +0800

    Create test case for DF_Role_RoleID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Role_RoleID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Role_RoleID]
+AS
+BEGIN
+    -- Create dummy data for Role.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Role]', @Defaults=1;
+
+    DECLARE @RoleName NVARCHAR(50) = 'Role Name 01';
+
+    INSERT INTO [dbo].[Role] (RoleName) VALUES
+    (@RoleName);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = RoleID from [dbo].[Role];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
