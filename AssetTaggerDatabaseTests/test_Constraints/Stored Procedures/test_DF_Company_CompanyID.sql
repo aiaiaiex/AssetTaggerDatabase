4a6c57253201a07027c5cb3d4b66e48d88d3bd51
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-23 16:41:34 +0800

    Create test case for DF_Company_CompanyID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Company_CompanyID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Company_CompanyID]
+AS
+BEGIN
+    -- Create dummy data for Company.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Company]', @Defaults=1;
+
+    DECLARE @CompanyName NVARCHAR(50) = 'Company Name 01';
+
+    INSERT INTO [dbo].[Company] (CompanyName) VALUES
+    (@CompanyName);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = CompanyID from [dbo].[Company];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
