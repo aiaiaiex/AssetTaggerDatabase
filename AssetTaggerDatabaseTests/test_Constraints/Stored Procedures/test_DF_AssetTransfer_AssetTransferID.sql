37def10f2073b4abf1bcce6703b34cf5e8c65411
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-23 16:04:17 +0800

    Create test case for DF_AssetTransfer_AssetTransferID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_AssetTransfer_AssetTransferID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_AssetTransfer_AssetTransferID]
+AS
+BEGIN
+    -- Create dummy data for AssetTransfer.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[AssetTransfer]', @Defaults=1;
+
+    DECLARE @AssetTransferDate DATETIME = '2000-01-01';
+
+    INSERT INTO [dbo].[AssetTransfer] (AssetTransferDate) VALUES
+    (@AssetTransferDate);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = AssetTransferID from [dbo].[AssetTransfer];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
