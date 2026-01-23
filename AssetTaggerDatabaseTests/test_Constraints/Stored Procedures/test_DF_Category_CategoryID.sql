a7cfed7b01add749d6d3fc3fa4a382c937146fb0
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-23 16:08:15 +0800

    Create test case for DF_Category_CategoryID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Category_CategoryID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Category_CategoryID]
+AS
+BEGIN
+    -- Create dummy data for Category.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Category]', @Defaults=1;
+
+    DECLARE @CategoryName NVARCHAR(50) = 'Category Name 01';
+
+    INSERT INTO [dbo].[Category] (CategoryName) VALUES
+    (@CategoryName);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = CategoryID from [dbo].[Category];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
