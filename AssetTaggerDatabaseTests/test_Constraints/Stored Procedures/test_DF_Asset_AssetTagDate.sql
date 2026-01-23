1297c2dae017611154b20e335c288b2309013840
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-23 15:11:58 +0800

    Create test case for DF_Asset_AssetID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Asset_AssetTagDate.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Asset_AssetTagDate]
+AS
+BEGIN
+    -- Create dummy data for Asset.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Asset]', @Defaults=1;
+
+    DECLARE @AssetPurchaseDate DATETIME = '2000-01-01';
+
+    INSERT INTO [dbo].[Asset] (AssetPurchaseDate) VALUES
+    (@AssetPurchaseDate);
+
+    -- Actual ouput.
+    DECLARE @actual DATETIME;
+    SELECT @actual = AssetTagDate from [dbo].[Asset];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
