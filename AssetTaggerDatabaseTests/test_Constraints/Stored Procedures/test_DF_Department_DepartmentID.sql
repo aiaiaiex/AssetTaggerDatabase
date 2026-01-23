2bab9c97ff7c8a69af30af61a5c701d3ef8fe608
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-23 16:44:31 +0800

    Create test case for DF_Department_DepartmentID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Department_DepartmentID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Department_DepartmentID]
+AS
+BEGIN
+    -- Create dummy data for Department.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Department]', @Defaults=1;
+
+    DECLARE @DepartmentName NVARCHAR(50) = 'Department Name 01';
+
+    INSERT INTO [dbo].[Department] (DepartmentName) VALUES
+    (@DepartmentName);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = DepartmentID from [dbo].[Department];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
