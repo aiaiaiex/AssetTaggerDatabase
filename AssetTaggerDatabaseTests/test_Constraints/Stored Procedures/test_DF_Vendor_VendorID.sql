bfc0d30e669c7ac7bce75a3196de6527eb1c1912
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-23 17:00:05 +0800

    Create test case for DF_Vendor_VendorID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Vendor_VendorID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Vendor_VendorID]
+AS
+BEGIN
+    -- Create dummy data for Vendor.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Vendor]', @Defaults=1;
+
+    DECLARE @VendorName NVARCHAR(50) = 'Vendor Name 01';
+
+    INSERT INTO [dbo].[Vendor] (VendorName) VALUES
+    (@VendorName);
+
+    -- Actual output.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = VendorID from [dbo].[Vendor];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
