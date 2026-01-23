2d8d985bbc9558b63f80c906583fe28003b2d969
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-23 14:13:10 +0800

    Change prefix of constraints concerning two or more columns to CTK

--- AssetTaggerDatabase/dbo/Tables/AssetFix.sql
+++ AssetTaggerDatabase/dbo/Tables/AssetFix.sql
@@ -10,7 +10,7 @@
     [EmployeeID]        UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_AssetFIx] PRIMARY KEY CLUSTERED ([AssetFixID] ASC),
     CONSTRAINT [CK_AssetFix_AssetFixCost] CHECK ([AssetFixCost]>=(0)),
-    CONSTRAINT [CK_AssetFix_AssetFixDateEnd_AssetFixDateStart] CHECK ([AssetFixDateEnd]>=[AssetFixDateStart]),
+    CONSTRAINT [CTK_AssetFix_AssetFixDateEnd_AssetFixDateStart] CHECK ([AssetFixDateEnd]>=[AssetFixDateStart]),
     CONSTRAINT [FK_AssetFix_AssetIssue] FOREIGN KEY ([AssetIssueID]) REFERENCES [dbo].[AssetIssue] ([AssetIssueID]),
     CONSTRAINT [FK_AssetFix_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID])
 );
