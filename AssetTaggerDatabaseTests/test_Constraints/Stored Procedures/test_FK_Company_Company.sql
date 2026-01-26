9fbded75ccaadd7bf532f2acf72f5dcb7d8c8c00
Guiliani <guilianipobre11@gmail.com>
2026-01-26 13:34:21 +0800

    Create test_FK_Company_Company constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_Company_Company.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_Company_Company]
+AS
+BEGIN
+    -- Create dummy data for Company.
+    EXEC tSQLt.FakeTable '[dbo].[Company]';
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Company]', '[FK_Company_Company]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[Company] (ParentCompanyID) VALUES
+    (NEWID());
+END;
