f58a38a03d2806f90b97894de446ded1b62044ae
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 11:25:29 +0800

    Create test case for FK_Asset_Location

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_Asset_Location.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_Asset_Location]
+AS
+BEGIN
+    -- Create dummy data for Asset.
+    EXEC tSQLt.FakeTable '[dbo].[Asset]';
+
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Asset]', '[FK_Asset_Location]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[Asset] (LocationID) VALUES
+    (NEWID());
+END;
