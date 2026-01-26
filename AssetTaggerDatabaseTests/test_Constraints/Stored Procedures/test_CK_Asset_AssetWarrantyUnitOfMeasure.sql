79646c0666d355d7f7933e61c15ca5fdb08b5e45
Guiliani <guilianipobre11@gmail.com>
2026-01-26 14:10:36 +0800

    Create test_CK_Asset_AssetWarrantyUnitOfMeasure constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_CK_Asset_AssetWarrantyUnitOfMeasure.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_CK_Asset_AssetWarrantyUnitOfMeasure]
+AS
+BEGIN
+    -- Create dummy data for Asset.
+    EXEC tSQLt.FakeTable '[dbo].[Asset]';
+
+    -- Apply check constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Asset]', '[CK_Asset_AssetWarrantyUnitOfMeasure]';
+    -- Test check constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting unacceptable data.
+    INSERT INTO [dbo].[Asset] (AssetWarrantyUnitOfMeasure) VALUES
+    ('JD');
+END;
