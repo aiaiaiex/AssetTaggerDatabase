c5921fe02f7df686264a8d9dea77c2594203b0ca
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 14:46:13 +0800

    Create test case for CTK_Company_CompanyID_ParentCompanyID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_CTK_Company_CompanyID_ParentCompanyID.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_CTK_Company_CompanyID_ParentCompanyID]
+AS
+BEGIN
+    -- Create dummy data for Company.
+    EXEC tSQLt.FakeTable '[dbo].[Company]';
+
+    DECLARE @CompanyID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply check constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Company]', '[CTK_Company_CompanyID_ParentCompanyID]';
+
+    -- Test check constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting unacceptable data.
+    INSERT INTO [dbo].[Company] (CompanyID, ParentCompanyID) VALUES
+    (@CompanyID, @CompanyID);
+END;
