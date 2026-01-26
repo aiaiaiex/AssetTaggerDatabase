7a05f6108a49dc13e0ff2039f46b2ddd71c12faa
Guiliani <guilianipobre11@gmail.com>
2026-01-26 10:16:50 +0800

    Create test_AK_EndUser_EndUserName constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_AK_EndUser_EndUserName.sql
@@ -0,0 +1,20 @@
+﻿
+
+CREATE PROCEDURE [test_Constraints].[test_AK_EndUser_EndUserName]
+AS
+BEGIN
+    -- Create dummy data for EndUser.
+    EXEC tSQLt.FakeTable '[dbo].[EndUser]';
+    DECLARE @EndUserName NVARCHAR(50) = 'End User Name 1';
+
+    -- Apply unique (alternate key) constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[EndUser]', '[AK_EndUser_EndUserName]';
+
+    -- Test unique (alternate key) constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) data twice.
+    INSERT INTO [dbo].[EndUser] (EndUserName) VALUES
+    (@EndUserName),
+    (@EndUserName);
+END;
