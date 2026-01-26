af69e5b2256ec3a56f3d73a21daf6c39b5a358d1
Guiliani <guilianipobre11@gmail.com>
2026-01-26 09:19:29 +0800

    Create test_PK_Building constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_PK_Building.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_PK_Building]
+AS
+BEGIN
+    -- Create dummy data for Building.
+    EXEC tSQLt.FakeTable '[dbo].[Building]';
+
+    DECLARE @BuildingID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply primary key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Building]', '[PK_Building]';
+
+    -- Test primary key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) ID twice.
+    INSERT INTO [dbo].[Building] (BuildingID) VALUES
+    (@BuildingID),
+    (@BuildingID);
+END;
