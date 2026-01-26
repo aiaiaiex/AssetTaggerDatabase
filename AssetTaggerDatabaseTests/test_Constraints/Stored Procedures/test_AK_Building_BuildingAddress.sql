cfccd2ba0a041125b94b71a565053ad3a942eeb8
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 09:38:17 +0800

    Create test case for AK_Building_BuildingAddress

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_AK_Building_BuildingAddress.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_AK_Building_BuildingAddress]
+AS
+BEGIN
+    -- Create dummy data for Building.
+    EXEC tSQLt.FakeTable '[dbo].[Building]';
+
+    DECLARE @BuildingAddress NVARCHAR(50) = 'Building Address 01';
+
+    -- Apply unique (alternate key) constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Building]', '[AK_Building_BuildingAddress]';
+
+    -- Test unique (alternate key) constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) data twice.
+    INSERT INTO [dbo].[Building] (BuildingAddress) VALUES
+    (@BuildingAddress),
+    (@BuildingAddress);
+END;
