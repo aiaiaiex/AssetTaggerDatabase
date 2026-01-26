4e7ed51e04d0dab2003d0cd5aeaae091d1d98fb5
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 14:22:21 +0800

    Create test case for CTK_AssetFix_AssetFixDateEnd_AssetFixDateStart

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_CTK_AssetFix_AssetFixDateEnd_AssetFixDateStart.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_CTK_AssetFix_AssetFixDateEnd_AssetFixDateStart]
+AS
+BEGIN
+    -- Create dummy data for AssetFix.
+    EXEC tSQLt.FakeTable '[dbo].[AssetFix]';
+
+    -- Apply check constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[AssetFix]', '[CTK_AssetFix_AssetFixDateEnd_AssetFixDateStart]';
+
+    -- Test check constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting unacceptable data.
+    INSERT INTO [dbo].[AssetFix] (AssetFixDateEnd, AssetFixDateStart) VALUES
+    ('2000-01-01','2000-01-02');
+END;
