5a195553e4aeb1128e0afa2b3b5467af708f0632
Guiliani <guilianipobre11@gmail.com>
2026-01-26 08:46:00 +0800

    Create test_PK_AssetFix constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_PK_AssetFix.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_PK_AssetFix]
+AS
+BEGIN
+    -- Create dummy data for AssetFix.
+    EXEC tSQLt.FakeTable '[dbo].[AssetFix]';
+
+    DECLARE @AssetFixID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply primary key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[AssetFix]', '[PK_AssetFix]';
+
+    -- Test primary key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) ID twice.
+    INSERT INTO [dbo].[AssetFix] (AssetFixID) VALUES
+    (@AssetFixID),
+    (@AssetFixID);
+END;
