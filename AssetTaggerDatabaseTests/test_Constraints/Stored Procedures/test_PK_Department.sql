be232df0ba879ea7ece01a526d16c26220c7faa7
Guiliani <guilianipobre11@gmail.com>
2026-01-26 09:28:11 +0800

    Create test_PK_Department constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_PK_Department.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_PK_Department]
+AS
+BEGIN
+    -- Create dummy data for Department.
+    EXEC tSQLt.FakeTable '[dbo].[Department]';
+
+    DECLARE @DepartmentID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply primary key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Department]', '[PK_Department]';
+
+    -- Test primary key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) ID twice.
+    INSERT INTO [dbo].[Department] (DepartmentID) VALUES
+    (@DepartmentID),
+    (@DepartmentID);
+END;
