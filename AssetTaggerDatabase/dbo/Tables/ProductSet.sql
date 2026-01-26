1a9cd74683917d25329390cf467361e3f94b3e58
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 15:11:54 +0800

    Create check constraint to prevent a product being its own parent

--- AssetTaggerDatabase/dbo/Tables/ProductSet.sql
+++ AssetTaggerDatabase/dbo/Tables/ProductSet.sql
@@ -3,6 +3,7 @@
     [ProductID]       UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_ProductSet] PRIMARY KEY CLUSTERED ([ParentProductID] ASC, [ProductID] ASC),
     CONSTRAINT [FK_ProductSet_Product_ParentProductID] FOREIGN KEY ([ParentProductID]) REFERENCES [dbo].[Product] ([ProductID]),
-    CONSTRAINT [FK_ProductSet_Product_ProductID] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID])
+    CONSTRAINT [FK_ProductSet_Product_ProductID] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Product] ([ProductID]),
+    CONSTRAINT [CTK_ProductSet_ParentProductID_ProductID] CHECK ([ParentProductID] <> [ProductID])
 );
 
