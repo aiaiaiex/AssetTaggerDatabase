49924eed8a5272d0995475fec482586a6aa68c42
Guiliani <guilianipobre11@gmail.com>
2026-01-26 08:49:37 +0800

    Create test_PK_AssetTransfer constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_PK_AssetTransfer.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_PK_AssetTransfer]
+AS
+BEGIN
+    -- Create dummy data for AssetTransfer.
+    EXEC tSQLt.FakeTable '[dbo].[AssetTransfer]';
+
+    DECLARE @AssetTransferID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply primary key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[AssetTransfer]', '[PK_AssetTransfer]';
+
+    -- Test primary key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) ID twice.
+    INSERT INTO [dbo].[AssetTransfer] (AssetTransferID) VALUES
+    (@AssetTransferID),
+    (@AssetTransferID);
+END;
