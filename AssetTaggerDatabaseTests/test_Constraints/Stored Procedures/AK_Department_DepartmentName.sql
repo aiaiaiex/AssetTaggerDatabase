9be937cd7a256266affee12fd8358da5f9fd0568
Guiliani <guilianipobre11@gmail.com>
2026-01-26 09:47:50 +0800

    Create AK_Department_DepartmentName constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/AK_Department_DepartmentName.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[AK_Department_DepartmentName]
+AS
+BEGIN
+    -- Create dummy data for Department.
+    EXEC tSQLt.FakeTable '[dbo].[Department]';
+
+    DECLARE @DepartmentName NVARCHAR(50) = 'Department Name 1';
+
+    -- Apply unique (alternate key) constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Department]', '[AK_Department_DepartmentName]';
+
+    -- Test unique (alternate key) constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) data twice.
+    INSERT INTO [dbo].[Department] (DepartmentName) VALUES
+    (@DepartmentName),
+    (@DepartmentName);
+END;
