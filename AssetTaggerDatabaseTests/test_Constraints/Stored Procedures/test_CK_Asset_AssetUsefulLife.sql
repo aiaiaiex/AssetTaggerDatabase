ad75e8e115d144debd23b12682b9b6a3272f6e8f
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 14:02:43 +0800

    Create test case for CK_Asset_AssetUsefulLife

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_CK_Asset_AssetUsefulLife.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_CK_Asset_AssetUsefulLife]
+AS
+BEGIN
+    -- Create dummy data for Asset.
+    EXEC tSQLt.FakeTable '[dbo].[Asset]';
+
+    -- Apply check constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Asset]', '[CK_Asset_AssetUsefulLife]';
+
+    -- Test check constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting unacceptable data.
+    INSERT INTO [dbo].[Asset] (AssetUsefulLife) VALUES
+    (-1);
+END;
