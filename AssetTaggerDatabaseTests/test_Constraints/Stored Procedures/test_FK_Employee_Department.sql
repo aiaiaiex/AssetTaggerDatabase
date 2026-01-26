ec475691ff3e4ae172d6622397eec3c8633f823e
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-26 13:23:53 +0800

    Create test case for FK_Employee_Department

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_Employee_Department.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_Employee_Department]
+AS
+BEGIN
+    -- Create dummy data for Employee.
+    EXEC tSQLt.FakeTable '[dbo].[Employee]';
+
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Employee]', '[FK_Employee_Department]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[Employee] (DepartmentID) VALUES
+    (NEWID());
+END;
