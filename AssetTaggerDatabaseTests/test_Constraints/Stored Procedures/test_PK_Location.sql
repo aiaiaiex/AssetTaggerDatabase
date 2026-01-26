2807b89875c19b50e78531bc65c53ea07699b299
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 09:06:35 +0800

    Create test case for PK_Location

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_PK_Location.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_PK_Location]
+AS
+BEGIN
+    -- Create dummy data for Location.
+    EXEC tSQLt.FakeTable '[dbo].[Location]';
+
+    DECLARE @LocationID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply primary key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Location]', '[PK_Location]';
+
+    -- Test primary key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) ID twice.
+    INSERT INTO [dbo].[Location] (LocationID) VALUES
+    (@LocationID),
+    (@LocationID);
+END;
