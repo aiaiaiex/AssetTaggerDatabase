cf743eada55691d3cc2c1100051c71610d330808
Guiliani <guilianipobre11@gmail.com>
2026-01-26 14:13:07 +0800

    Create test_CK_AssetFix_AssetFixCost constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_CK_AssetFix_AssetFixCost.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_CK_AssetFix_AssetFixCost]
+AS
+BEGIN
+    -- Create dummy data for AssetFix.
+    EXEC tSQLt.FakeTable '[dbo].[AssetFix]';
+
+    -- Apply check constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[AssetFix]', '[CK_AssetFix_AssetFixCost]';
+    -- Test check constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting unacceptable data.
+    INSERT INTO [dbo].[AssetFix] (AssetFixCost) VALUES
+    (-1);
+END;
