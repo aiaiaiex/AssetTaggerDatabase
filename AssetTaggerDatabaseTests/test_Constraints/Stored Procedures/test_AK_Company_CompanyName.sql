c268022e9ff6fd138a1db26577e3ad902a78939f
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-26 09:45:34 +0800

    Create test case for AK_Company_CompanyName

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_AK_Company_CompanyName.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_AK_Company_CompanyName]
+AS
+BEGIN
+    -- Create dummy data for Company.
+    EXEC tSQLt.FakeTable '[dbo].[Company]';
+
+    DECLARE @CompanyName NVARCHAR(50) = 'Company Name 01';
+
+    -- Apply unique (alternate key) constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Company]', '[AK_Company_CompanyName]';
+
+    -- Test unique (alternate key) constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) data twice.
+    INSERT INTO [dbo].[Company] (CompanyName) VALUES
+    (@CompanyName),
+    (@CompanyName);
+END;
