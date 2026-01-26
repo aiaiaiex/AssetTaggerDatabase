eb6ab098c513c683512850ea05ae5f2974d05e02
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-26 09:17:00 +0800

    Create test case for PK_Vendor

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_PK_Vendor.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_PK_Vendor]
+AS
+BEGIN
+    -- Create dummy data for Vendor.
+    EXEC tSQLt.FakeTable '[dbo].[Vendor]';
+
+    DECLARE @VendorID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply primary key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Vendor]', '[PK_Vendor]';
+
+    -- Test primary key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) ID twice.
+    INSERT INTO [dbo].[Vendor] (VendorID) VALUES
+    (@VendorID),
+    (@VendorID);
+END;
