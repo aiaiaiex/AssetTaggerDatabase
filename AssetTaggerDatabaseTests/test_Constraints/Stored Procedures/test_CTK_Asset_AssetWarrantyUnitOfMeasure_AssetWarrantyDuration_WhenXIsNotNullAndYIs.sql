19e7e9c5c7db8335fa606be4172b9b98dfc85212
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 14:18:14 +0800

    Create test cases for CTK_Asset_AssetWarrantyUnitOfMeasure_AssetWarrantyDuration

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_CTK_Asset_AssetWarrantyUnitOfMeasure_AssetWarrantyDuration_WhenXIsNotNullAndYIs.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_CTK_Asset_AssetWarrantyUnitOfMeasure_AssetWarrantyDuration_WhenXIsNotNullAndYIs]
+AS
+BEGIN
+    -- Create dummy data for Asset.
+    EXEC tSQLt.FakeTable '[dbo].[Asset]';
+
+    -- Apply check constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Asset]', '[CTK_Asset_AssetWarrantyUnitOfMeasure_AssetWarrantyDuration]';
+
+    -- Test check constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting unacceptable data.
+    INSERT INTO [dbo].[Asset] (AssetWarrantyUnitOfMeasure, AssetWarrantyDuration) VALUES
+    ('yy', NULL);
+END;
