b1200448262a9488a86b9a34018638369d1a2499
Guiliani <guilianipobre11@gmail.com>
2026-01-26 13:41:31 +0800

    Create test_FK_Product_Category constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_Product_Category.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_Product_Category]
+AS
+BEGIN
+    -- Create dummy data for Product.
+    EXEC tSQLt.FakeTable '[dbo].[Product]';
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Product]', '[FK_Product_Category]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[Product] (CategoryID) VALUES
+    (NEWID());
+END;
