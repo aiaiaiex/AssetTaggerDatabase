887ea65c419633d1b41add2880d982396cf4ce95
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-26 13:36:22 +0800

    Create test case for FK_ProductSet_Product_ParentProductID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_ProductSet_Product_ParentProductID.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_ProductSet_Product_ParentProductID]
+AS
+BEGIN
+    -- Create dummy data for ProductSet.
+    EXEC tSQLt.FakeTable '[dbo].[ProductSet]';
+
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[ProductSet]', '[FK_ProductSet_Product_ParentProductID]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[ProductSet] (ParentProductID) VALUES
+    (NEWID());
+END;
