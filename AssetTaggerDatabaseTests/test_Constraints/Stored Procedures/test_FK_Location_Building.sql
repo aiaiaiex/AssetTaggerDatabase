0a473e1ca6f6d173bf67e60a949662b677ac61b5
Guiliani <guilianipobre11@gmail.com>
2026-01-26 13:38:18 +0800

    Create test_FK_Location_Building constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_Location_Building.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_Location_Building]
+AS
+BEGIN
+    -- Create dummy data for Location.
+    EXEC tSQLt.FakeTable '[dbo].[Location]';
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Location]', '[FK_Location_Building]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[Location] (BuildingID) VALUES
+    (NEWID());
+END;
