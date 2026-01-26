54cb3428f3f58309ab5622db88ee08d586880097
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 10:12:29 +0800

    Create test case for AK_Location_LocationAddress_BuildingID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_AK_Location_LocationAddress_BuildingID.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_AK_Location_LocationAddress_BuildingID]
+AS
+BEGIN
+    -- Create dummy data for Location.
+    EXEC tSQLt.FakeTable '[dbo].[Location]';
+
+    DECLARE @LocationAddress NVARCHAR(50) = 'Location Address 01';
+    DECLARE @BuildingID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply unique (alternate key) constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Location]', '[AK_Location_LocationAddress_BuildingID]';
+
+    -- Test unique (alternate key) constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) data twice.
+    INSERT INTO [dbo].[Location] (LocationAddress, BuildingID) VALUES
+    (@LocationAddress, @BuildingID),
+    (@LocationAddress, @BuildingID);
+END;
