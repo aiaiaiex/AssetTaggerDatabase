3d8b7e7b23850254e1520f99607729a78cbc38e6
Guiliani <guilianipobre11@gmail.com>
2026-01-26 10:23:44 +0800

    Create test_AK_Role_RoleName constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_AK_Role_RoleName.sql
@@ -0,0 +1,20 @@
+﻿
+
+CREATE PROCEDURE [test_Constraints].[test_AK_Role_RoleName]
+AS
+BEGIN
+    -- Create dummy data for Role.
+    EXEC tSQLt.FakeTable '[dbo].[Role]';
+    DECLARE @RoleName NVARCHAR(50) = 'Role Name 1';
+
+    -- Apply unique (alternate key) constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Role]', '[AK_Role_RoleName]';
+
+    -- Test unique (alternate key) constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) data twice.
+    INSERT INTO [dbo].[Role] (RoleName) VALUES
+    (@RoleName),
+    (@RoleName);
+END;
