32651b818569970176bcad430c6ad5f183364675
Guiliani <guilianipobre11@gmail.com>
2026-01-26 11:50:17 +0800

    Create test_FK_AssetIssue_Employee constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_AssetIssue_Employee.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_AssetIssue_Employee]
+AS
+BEGIN
+    -- Create dummy data for AssetIssue.
+    EXEC tSQLt.FakeTable '[dbo].[AssetIssue]';
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[AssetIssue]', '[FK_AssetIssue_Employee]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[AssetIssue] (EmployeeID) VALUES
+    (NEWID());
+END;
