40822ef2cc141cca60328656da2fda02d2cd0b01
Guiliani <guilianipobre11@gmail.com>
2026-01-26 14:07:39 +0800

    Create test_CK_Asset_AssetWarrantyDuration constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_CK_Asset_AssetWarrantyDuration.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_CK_Asset_AssetWarrantyDuration]
+AS
+BEGIN
+    -- Create dummy data for Asset.
+    EXEC tSQLt.FakeTable '[dbo].[Asset]';
+
+    -- Apply check constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Asset]', '[CK_Asset_AssetWarrantyDuration]';
+
+    -- Test check constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting unacceptable data.
+    INSERT INTO [dbo].[Asset] (AssetWarrantyDuration) VALUES
+    (-1);
+END;
