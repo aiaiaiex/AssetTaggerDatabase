9eb69acca067ac6c4aef91136c1b10aeb880990c
Guiliani <guilianipobre11@gmail.com>
2026-01-26 10:38:32 +0800

    Create test_AK_Category_CategoryName constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_AK_Category_CategoryName.sql
@@ -0,0 +1,20 @@
+﻿
+
+CREATE PROCEDURE [test_Constraints].[test_AK_Category_CategoryName]
+AS
+BEGIN
+    -- Create dummy data for Category.
+    EXEC tSQLt.FakeTable '[dbo].[Category]';
+    DECLARE @CategoryName NVARCHAR(50) = 'Category Name 1';
+
+    -- Apply unique (alternate key) constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Category]', '[AK_Category_CategoryName]';
+
+    -- Test unique (alternate key) constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) data twice.
+    INSERT INTO [dbo].[Category] (CategoryName) VALUES
+    (@CategoryName),
+    (@CategoryName);
+END;
