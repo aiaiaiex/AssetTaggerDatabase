b97480f2043ce3d675705f8f4cfe776ae724b5be
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 09:00:20 +0800

    Create test case for PK_AssetIssue

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_PK_AssetIssue.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_PK_AssetIssue]
+AS
+BEGIN
+    -- Create dummy data for AssetIssue.
+    EXEC tSQLt.FakeTable '[dbo].[AssetIssue]';
+
+    DECLARE @AssetIssueID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply primary key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[AssetIssue]', '[PK_AssetIssue]';
+
+    -- Test primary key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) ID twice.
+    INSERT INTO [dbo].[AssetIssue] (AssetIssueID) VALUES
+    (@AssetIssueID),
+    (@AssetIssueID);
+END;
