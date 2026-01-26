b2a87098b0d822d287dd069f8ef061d10f874847
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-26 11:57:15 +0800

    Create test case for FK_AssetTransfer_Company_CompanyID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_AssetTransfer_Company_CompanyID.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_AssetTransfer_Company_CompanyID]
+AS
+BEGIN
+    -- Create dummy data for AssetTransfer.
+    EXEC tSQLt.FakeTable '[dbo].[AssetTransfer]';
+
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[AssetTransfer]', '[FK_AssetTransfer_Company_CompanyID]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[AssetTransfer] (CompanyID) VALUES
+    (NEWID());
+END;
