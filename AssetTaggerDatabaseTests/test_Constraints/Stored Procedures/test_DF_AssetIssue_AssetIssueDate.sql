1bad612944325a883c1f98c067c8a4c503081496
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-27 15:07:48 +0800

    Create test case for DF_AssetIssue_AssetIssueDate

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_AssetIssue_AssetIssueDate.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_AssetIssue_AssetIssueDate]
+AS
+BEGIN
+    -- Create dummy data for AssetIssue.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[AssetIssue]', @Defaults=1;
+
+    DECLARE @AssetIssueTitle NVARCHAR(50) = 'Asset Issue Title 01';
+
+    INSERT INTO [dbo].[AssetIssue] (AssetIssueTitle) VALUES
+    (@AssetIssueTitle);
+
+    -- Actual ouput.
+    DECLARE @actual DATETIME;
+    SELECT @actual = AssetIssueDate from [dbo].[AssetIssue];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
