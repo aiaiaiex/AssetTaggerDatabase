501301df721b9b1f97cbdc534887edfdb87bc34f
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-27 15:47:12 +0800

    Create new foreign column to connect EndUser to Employee

--- AssetTaggerDatabase/dbo/Tables/EndUser.sql
+++ AssetTaggerDatabase/dbo/Tables/EndUser.sql
@@ -2,6 +2,8 @@
     [EndUserID]           UNIQUEIDENTIFIER CONSTRAINT [DF_EndUser_EndUserID] DEFAULT (newid()) NOT NULL,
     [EndUserName]         NVARCHAR (50)    NOT NULL,
     [EndUserPasswordHash] NCHAR (32)       NOT NULL,
+    [EmployeeID]          UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [AK_EndUser_EndUserName] UNIQUE ([EndUserName]),
-    CONSTRAINT [PK_EndUser] PRIMARY KEY CLUSTERED ([EndUserID] ASC)
+    CONSTRAINT [PK_EndUser] PRIMARY KEY CLUSTERED ([EndUserID] ASC),
+    CONSTRAINT [FK_EndUser_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID])
 );
