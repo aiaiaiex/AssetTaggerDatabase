421e75add8076eb25f4740e8ce9b090af8a3df8c
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-27 15:06:02 +0800

    Create default constraint for AssetIssueDate

--- AssetTaggerDatabase/dbo/Tables/AssetIssue.sql
+++ AssetTaggerDatabase/dbo/Tables/AssetIssue.sql
@@ -2,7 +2,7 @@
     [AssetIssueID]    UNIQUEIDENTIFIER CONSTRAINT [DF_AssetIssue_AssetIssueID] DEFAULT (newid()) NOT NULL,
     [AssetIssueTitle] NVARCHAR (50)    NOT NULL,
     [AssetIssueDesc]  NTEXT            NULL,
-    [AssetIssueDate]  DATETIME         NULL,
+    [AssetIssueDate]  DATETIME         CONSTRAINT [DF_AssetIssue_AssetIssueDate] DEFAULT (GETDATE()) NOT NULL,
     [AssetID]         UNIQUEIDENTIFIER NOT NULL,
     [EmployeeID]      UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_AssetIssue] PRIMARY KEY CLUSTERED ([AssetIssueID] ASC),
