00665d6578723b52520920e97eb87f8338eebaea
Guiliani <guilianipobre11@gmail.com>
2026-01-27 15:10:15 +0800

    Create test_DF_AssetFix_AssetFixDateStart constraint test

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_AssetFix_AssetFixDateStart.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_AssetFix_AssetFixDateStart]
+AS
+BEGIN
+    -- Create dummy data for AssetFix.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[AssetFix]', @Defaults=1;
+
+    DECLARE @AssetFixTitle NVARCHAR(50) = 'Hardware Repair';
+
+    INSERT INTO [dbo].[AssetFix] (AssetFixTitle) VALUES
+    (@AssetFixTitle);
+
+    -- Actual ouput.
+    DECLARE @actual DATETIME;
+    SELECT @actual = AssetFixDateStart from [dbo].[AssetFix];
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
