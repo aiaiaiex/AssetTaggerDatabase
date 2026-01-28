81754d488db9cdfa2d610f9a3baeaca6f74b3f77
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-28 10:27:32 +0800

    Create test case for FK_EndUser_EndUserRole

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_EndUser_EndUserRole.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_EndUser_EndUserRole]
+AS
+BEGIN
+    -- Create dummy data for EndUser.
+    EXEC tSQLt.FakeTable '[dbo].[EndUser]';
+
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[EndUser]', '[FK_EndUser_EndUserRole]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[EndUser] (EndUserRoleID) VALUES
+    (NEWID());
+END;
