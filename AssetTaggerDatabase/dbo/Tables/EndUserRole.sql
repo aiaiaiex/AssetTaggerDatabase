6ebe97470d880ca558311f1acc8af72f44b4ff05
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-28 09:20:44 +0800

    Create EndUserRole table

---
+++ AssetTaggerDatabase/dbo/Tables/EndUserRole.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[EndUserRole] (
+    [EndUserRoleID]   UNIQUEIDENTIFIER CONSTRAINT [DF_EndUserRole_EndUserRoleID] DEFAULT (newid()) NOT NULL,
+    [EndUserRoleName] NVARCHAR (50)    NOT NULL,
+    CONSTRAINT [PK_EndUserRole] PRIMARY KEY CLUSTERED ([EndUserRoleID] ASC),
+    CONSTRAINT [AK_EndUserRole_EndUserRoleName] UNIQUE NONCLUSTERED ([EndUserRoleName] ASC)
+);
+
