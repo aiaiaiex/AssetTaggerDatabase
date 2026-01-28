a8dc5d561832c64bbf7b2417cf0e1aca076d91fd
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-28 11:37:39 +0800

    Create new foreign column to connect Product to Manufacturer

--- AssetTaggerDatabase/dbo/Tables/Product.sql
+++ AssetTaggerDatabase/dbo/Tables/Product.sql
@@ -3,8 +3,10 @@
     [ProductName]         NVARCHAR (50)    NULL,
     [ProductModelNumber]  NVARCHAR (50)    NULL,
     [ProductManufacturer] NVARCHAR (50)    NULL,
+    [ManufacturerID]      UNIQUEIDENTIFIER NULL,
     [CategoryID]          UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [AK_Product_ProductModelNumber_ProductManufacturer] UNIQUE ([ProductModelNumber], [ProductManufacturer]),
     CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([ProductID] ASC),
+    CONSTRAINT [FK_Product_Manufacturer] FOREIGN KEY ([ManufacturerID]) REFERENCES [dbo].[Manufacturer] ([ManufacturerID]),
     CONSTRAINT [FK_Product_Category] FOREIGN KEY ([CategoryID]) REFERENCES [dbo].[Category] ([CategoryID])
 );
