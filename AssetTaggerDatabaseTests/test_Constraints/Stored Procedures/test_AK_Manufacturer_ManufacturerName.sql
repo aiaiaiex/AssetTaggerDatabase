82876a598121746d88895c8364006090128e477b
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-28 11:16:46 +0800

    Create test case for AK_Manufacturer_ManufacturerName

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_AK_Manufacturer_ManufacturerName.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_AK_Manufacturer_ManufacturerName]
+AS
+BEGIN
+    -- Create dummy data for Manufacturer.
+    EXEC tSQLt.FakeTable '[dbo].[Manufacturer]';
+
+    DECLARE @ManufacturerName NVARCHAR(50) = 'Manufacturer Name 01';
+
+    -- Apply unique (alternate key) constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Manufacturer]', '[AK_Manufacturer_ManufacturerName]';
+
+    -- Test unique (alternate key) constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) data twice.
+    INSERT INTO [dbo].[Manufacturer] (ManufacturerName) VALUES
+    (@ManufacturerName),
+    (@ManufacturerName);
+END;
