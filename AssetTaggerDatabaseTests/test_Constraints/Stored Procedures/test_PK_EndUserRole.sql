a7839aeadd41db7f2e66a2fdcf23ebb3e70d5a27
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-28 09:44:50 +0800

    Create test case for PK_EndUserRole

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_PK_EndUserRole.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_PK_EndUserRole]
+AS
+BEGIN
+    -- Create dummy data for EndUserRole.
+    EXEC tSQLt.FakeTable '[dbo].[EndUserRole]';
+
+    DECLARE @EndUserRoleID UNIQUEIDENTIFIER = NEWID();
+
+    -- Apply primary key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[EndUserRole]', '[PK_EndUserRole]';
+
+    -- Test primary key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 2627;
+
+    -- Create error by inserting same (unique) ID twice.
+    INSERT INTO [dbo].[EndUserRole] (EndUserRoleID) VALUES
+    (@EndUserRoleID),
+    (@EndUserRoleID);
+END;
