5ce700f393dec23cf75e49b8b6aff6cd3775f08f
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-28 13:26:50 +0800

    Replace ProductManufacturer with ManufacturerID

--- AssetTaggerDatabase/dbo/Tables/Product.sql
+++ AssetTaggerDatabase/dbo/Tables/Product.sql
@@ -2,10 +2,9 @@
     [ProductID]           UNIQUEIDENTIFIER CONSTRAINT [DF_Product_ProductID] DEFAULT (newid()) NOT NULL,
     [ProductName]         NVARCHAR (50)    NULL,
     [ProductModelNumber]  NVARCHAR (50)    NULL,
-    [ProductManufacturer] NVARCHAR (50)    NULL,
     [ManufacturerID]      UNIQUEIDENTIFIER NULL,
     [CategoryID]          UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT [AK_Product_ProductModelNumber_ProductManufacturer] UNIQUE ([ProductModelNumber], [ProductManufacturer]),
+    CONSTRAINT [AK_Product_ProductModelNumber_ManufacturerID] UNIQUE ([ProductModelNumber], [ManufacturerID]),
     CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([ProductID] ASC),
     CONSTRAINT [FK_Product_Manufacturer] FOREIGN KEY ([ManufacturerID]) REFERENCES [dbo].[Manufacturer] ([ManufacturerID]),
     CONSTRAINT [FK_Product_Category] FOREIGN KEY ([CategoryID]) REFERENCES [dbo].[Category] ([CategoryID])
