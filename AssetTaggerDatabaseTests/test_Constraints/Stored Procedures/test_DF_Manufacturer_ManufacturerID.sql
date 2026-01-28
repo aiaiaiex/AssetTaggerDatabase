39d8513347154180fabb6ad29fc9fb8caf34438f
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-01-28 11:06:31 +0800

    Create test case for DF_Manufacturer_ManufacturerID

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_DF_Manufacturer_ManufacturerID.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_DF_Manufacturer_ManufacturerID]
+AS
+BEGIN
+    -- Create dummy data for Manufacturer.
+    -- Preserve default constraints.
+    EXEC tSQLt.FakeTable '[dbo].[Manufacturer]', @Defaults=1;
+
+    DECLARE @ManufacturerName NVARCHAR(50) = 'Manufacturer Name 01';
+
+    INSERT INTO [dbo].[Manufacturer] (ManufacturerName) VALUES
+    (@ManufacturerName);
+
+    -- Actual ouput.
+    DECLARE @actual UNIQUEIDENTIFIER;
+    SELECT @actual = ManufacturerID from [dbo].[Manufacturer];
+
+    -- Check if default value is not null.
+    EXEC tSQLt.AssertNotEquals NULL, @actual;
+END;
