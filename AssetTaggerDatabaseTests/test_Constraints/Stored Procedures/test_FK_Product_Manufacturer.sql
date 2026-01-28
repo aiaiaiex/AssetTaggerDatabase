12949a01d6c0a3f5f09f52e6554c219727aca5b3
cbysais <217733506+cbysais@users.noreply.github.com>
2026-01-28 11:37:53 +0800

    Create test case for FK_Product_Manufacturer

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_FK_Product_Manufacturer.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_FK_Product_Manufacturer]
+AS
+BEGIN
+    -- Create dummy data for Product.
+    EXEC tSQLt.FakeTable '[dbo].[Product]';
+
+    -- Apply foreign key constraint.
+    EXEC tSQLt.ApplyConstraint '[dbo].[Product]', '[FK_Product_Manufacturer]';
+
+    -- Test foreign key constraint by expecting an error.
+    EXEC tSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting data not from foreign table.
+    INSERT INTO [dbo].[Product] (ManufacturerID) VALUES
+    (NEWID());
+END;
