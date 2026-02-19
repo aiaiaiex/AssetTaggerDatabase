87cf9e33f293c15d6592e09c1d956e5e357e73ff
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-19 10:46:59 +0800

    Add the nullable columns as optional parameters

--- AssetTaggerDatabase/dbo/Stored Procedures/usp_CreateEndUser.sql
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_CreateEndUser.sql
@@ -1,15 +1,19 @@
 ﻿CREATE PROCEDURE [dbo].[usp_CreateEndUser]
     @EndUserName NVARCHAR(50),
-    @EndUserPassword NVARCHAR(255)
+    @EndUserPassword NVARCHAR(255),
+    @EndUserRoleID UNIQUEIDENTIFIER = NULL,
+    @EmployeeID UNIQUEIDENTIFIER = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    INSERT INTO [dbo].[EndUser] (EndUserName, EndUserPasswordHash)
+    INSERT INTO [dbo].[EndUser] (EndUserName, EndUserPasswordHash, EndUserRoleID, EmployeeID)
     OUTPUT INSERTED.EndUserID, INSERTED.EndUserName, INSERTED.EndUserRoleID, INSERTED.EmployeeID
     VALUES (
         @EndUserName,
-        CONVERT(NCHAR(32), HASHBYTES('SHA2_256', @EndUserPassword))
-        -- CAST(HASHBYTES('SHA2_256', @EndUserPassword) AS NCHAR(32))
+        CONVERT(NCHAR(32), HASHBYTES('SHA2_256', @EndUserPassword)),
+        -- CAST(HASHBYTES('SHA2_256', @EndUserPassword) AS NCHAR(32)),
+        @EndUserRoleID,
+        @EmployeeID
     );
 END
