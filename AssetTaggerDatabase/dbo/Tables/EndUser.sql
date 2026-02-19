f2cd329dd85e80f629d8e7dbb657a8e742eb3092
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-19 08:53:35 +0800

    Rename CK_EndUser_EndUserName constraint to CK_EndUser_EndUserName_OnlyLowercase

--- AssetTaggerDatabase/dbo/Tables/EndUser.sql
+++ AssetTaggerDatabase/dbo/Tables/EndUser.sql
@@ -5,8 +5,8 @@
     [EndUserRoleID] UNIQUEIDENTIFIER NULL,
     [EmployeeID] UNIQUEIDENTIFIER NULL,
     CONSTRAINT [AK_EndUser_EndUserName] UNIQUE ([EndUserName]),
-    CONSTRAINT [CK_EndUser_EndUserName] CHECK (BINARY_CHECKSUM([EndUserName]) = BINARY_CHECKSUM(LOWER([EndUserName]))),
     CONSTRAINT [CK_EndUser_EndUserName_NoWhitespace] CHECK (CHARINDEX(' ', [EndUserName]) = 0),
+    CONSTRAINT [CK_EndUser_EndUserName_OnlyLowercase] CHECK (BINARY_CHECKSUM([EndUserName]) = BINARY_CHECKSUM(LOWER([EndUserName]))),
     CONSTRAINT [PK_EndUser] PRIMARY KEY CLUSTERED ([EndUserID] ASC),
     CONSTRAINT [FK_EndUser_EndUserRole] FOREIGN KEY ([EndUserRoleID]) REFERENCES [dbo].[EndUserRole] ([EndUserRoleID]),
     CONSTRAINT [FK_EndUser_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID])
