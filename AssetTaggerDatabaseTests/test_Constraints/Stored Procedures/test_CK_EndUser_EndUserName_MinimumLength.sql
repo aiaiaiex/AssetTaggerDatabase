73462b7b2ac70ca59e34dc322893328412498cf8
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-20 14:50:05 +0800

    Test CK_EndUser_EndUserName_MinimumLength constraint

---
+++ AssetTaggerDatabaseTests/test_Constraints/Stored Procedures/test_CK_EndUser_EndUserName_MinimumLength.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE PROCEDURE [test_Constraints].[test_CK_EndUser_EndUserName_MinimumLength]
+AS
+BEGIN
+    -- Create dummy data for EndUser.
+    EXEC TSQLt.FakeTable '[dbo].[EndUser]';
+
+    -- Apply check constraint.
+    EXEC TSQLt.ApplyConstraint '[dbo].[EndUser]', '[CK_EndUser_EndUserName_MinimumLength]';
+
+    -- Test check constraint by expecting an error.
+    EXEC TSQLt.ExpectException @ExpectedErrorNumber = 547;
+
+    -- Create error by inserting unacceptable data.
+    INSERT INTO [dbo].[EndUser] (EndUserName) VALUES ('');
+END;
