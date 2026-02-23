06964d3e4c902fc77342fa5bd8b2ed0e6f0dcb44
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-23 09:15:27 +0800

    Test usp_DeleteEndUser stored procedure

---
+++ AssetTaggerDatabaseTests/test_StoredProcedures/Stored Procedures/test_usp_DeleteEndUser.sql
@@ -0,0 +1,51 @@
+﻿CREATE PROCEDURE [test_StoredProcedures].[test_usp_DeleteEndUser]
+AS
+BEGIN
+    -- Create dummy data for EndUser.
+    EXEC TSQLt.FakeTable '[dbo].[EndUser]';
+
+    DECLARE @EndUserID01 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @EndUserID02 UNIQUEIDENTIFIER = NEWID();
+
+    DECLARE @EndUserName01 NVARCHAR(50) = 'End User Name 01';
+    DECLARE @EndUserName02 NVARCHAR(50) = 'End User Name 02';
+
+    DECLARE @EndUserRoleID01 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @EndUserRoleID02 UNIQUEIDENTIFIER = NEWID();
+
+    DECLARE @EmployeeID01 UNIQUEIDENTIFIER = NEWID();
+    DECLARE @EmployeeID02 UNIQUEIDENTIFIER = NEWID();
+
+    INSERT INTO [dbo].[EndUser] (EndUserID, EndUserName, EndUserRoleID, EmployeeID) VALUES
+    (@EndUserID01, @EndUserName01, @EndUserRoleID01, @EmployeeID01),
+    (@EndUserID02, @EndUserName02, @EndUserRoleID02, @EmployeeID02);
+
+    -- Actual output.
+    CREATE TABLE #actual (
+        EndUserID UNIQUEIDENTIFIER,
+        EndUserName NVARCHAR(50),
+        EndUserRoleID UNIQUEIDENTIFIER,
+        EmployeeID UNIQUEIDENTIFIER
+    );
+
+    INSERT INTO #actual EXEC [dbo].[usp_DeleteEndUser] @EndUserID01;
+
+    -- Expected output.
+    CREATE TABLE #expected (
+        EndUserID UNIQUEIDENTIFIER,
+        EndUserName NVARCHAR(50),
+        EndUserRoleID UNIQUEIDENTIFIER,
+        EmployeeID UNIQUEIDENTIFIER,
+    );
+
+    INSERT INTO #expected (EndUserID, EndUserName, EndUserRoleID, EmployeeID)
+    VALUES (
+        @EndUserID01,
+        @EndUserName01,
+        @EndUserRoleID01,
+        @EmployeeID01
+    );
+
+    -- Assert outputs.
+    EXEC TSQLt.AssertEqualsTable '#expected', '#actual';
+END;
