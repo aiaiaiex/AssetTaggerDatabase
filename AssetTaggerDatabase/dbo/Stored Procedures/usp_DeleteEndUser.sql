80f2745b97de78cf13b8154789ffd8443a9a0144
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-02-23 09:03:33 +0800

    Create usp_DeleteEndUser stored procedure

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_DeleteEndUser.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[usp_DeleteEndUser]
+    @EndUserID UNIQUEIDENTIFIER
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DELETE [dbo].[EndUser]
+    OUTPUT DELETED.EndUserID, DELETED.EndUserName, DELETED.EndUserRoleID, DELETED.EmployeeID
+    FROM [dbo].[EndUser]
+    WHERE EndUserID = @EndUserID;
+END
