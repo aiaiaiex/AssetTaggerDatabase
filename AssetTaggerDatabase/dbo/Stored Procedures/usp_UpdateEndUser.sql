91a1eb9d7a3eb2284aec0731c19c04ff7c107468
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-04 08:56:47 +0800

    Use nullish constants instead of null as default values of parameters used to update nullable columns

--- AssetTaggerDatabase/dbo/Stored Procedures/usp_UpdateEndUser.sql
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_UpdateEndUser.sql
@@ -1,37 +1,26 @@
 ﻿CREATE PROCEDURE [dbo].[usp_UpdateEndUser]
     @EndUserID UNIQUEIDENTIFIER,
     @EndUserName NVARCHAR(50) = NULL,
-    @EndUserRoleID UNIQUEIDENTIFIER = NULL,
-    @EmployeeID UNIQUEIDENTIFIER = NULL,
-    @NullifyEndUserRoleID BIT = 0,
-    @NullifyEmployeeID BIT = 0
+    @EndUserRoleID UNIQUEIDENTIFIER = '00000000-0000-0000-0000-000000000000',
+    @EmployeeID UNIQUEIDENTIFIER = '00000000-0000-0000-0000-000000000000'
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    -- Get CONSTANTS.
+    DECLARE @NULLISH_UNIQUEIDENTIFIER UNIQUEIDENTIFIER;
+    SELECT @NULLISH_UNIQUEIDENTIFIER = (SELECT NULLISH_UNIQUEIDENTIFIER FROM [dbo].[VI_NullishConstants]);
+
     -- Validate input.
-    IF (@EndUserName IS NULL AND @EndUserRoleID IS NULL AND @EmployeeID IS NULL AND @NullifyEndUserRoleID = 0 AND @NullifyEmployeeID = 0)
+    IF (@EndUserName IS NULL AND @EndUserRoleID = @NULLISH_UNIQUEIDENTIFIER AND @EmployeeID = @NULLISH_UNIQUEIDENTIFIER)
         BEGIN
             RAISERROR ('Cannot update row with @EndUserID when no non-default values are passed to other parameters!', 11, 0);
             RETURN -1;
         END
 
-    IF (@NullifyEndUserRoleID = 1 AND @EndUserRoleID IS NOT NULL)
-        BEGIN
-            RAISERROR ('Cannot nullify EndUserRoleID when @EndUserRoleID is not null!', 11, 0);
-            RETURN -1;
-        END
-
-    IF (@NullifyEmployeeID = 1 AND @EmployeeID IS NOT NULL)
-        BEGIN
-            RAISERROR ('Cannot nullify EmployeeID when @EmployeeID is not null!', 11, 0);
-            RETURN -1;
-        END
-
     -- Run actual query.
-
     UPDATE [dbo].[EndUser]
-    SET EndUserName = ISNULL(@EndUserName, EndUserName), EndUserRoleID = IIF(@NullifyEndUserRoleID = 1, NULL, ISNULL(@EndUserRoleID, EndUserRoleID)), EmployeeID = IIF(@NullifyEmployeeID = 1, NULL, ISNULL(@EmployeeID, EmployeeID))
+    SET EndUserName = ISNULL(@EndUserName, EndUserName), EndUserRoleID = IIF(@EndUserRoleID = @NULLISH_UNIQUEIDENTIFIER, EndUserRoleID, @EndUserRoleID), EmployeeID = IIF(@EmployeeID = @NULLISH_UNIQUEIDENTIFIER, EmployeeID, @EmployeeID)
     OUTPUT INSERTED.EndUserID, INSERTED.EndUserName, INSERTED.EndUserRoleID, INSERTED.EmployeeID, DELETED.EndUserName AS OldEndUserName, DELETED.EndUserRoleID AS OldEndUserRoleID, DELETED.EmployeeID AS OldEmployeeID
     FROM [dbo].[EndUser]
     WHERE EndUserID = @EndUserID;
