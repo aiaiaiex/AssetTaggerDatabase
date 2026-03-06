7376882fcdef49bc8ff9ee18369a1dc17c77006c
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-06 09:20:36 +0800

    Add comment on why NULLISH_UNIQUEIDENTIFIER will never be equal to UNIQUEIDENTIFIERs generated by NEWID()

--- AssetTaggerDatabase/dbo/Views/VI_NullishConstants.sql
+++ AssetTaggerDatabase/dbo/Views/VI_NullishConstants.sql
@@ -1,5 +1,9 @@
 ﻿CREATE VIEW [dbo].[VI_NullishConstants]
 AS
 SELECT
+    -- @NULLISH_UNIQUEIDENTIFIER (00000000-0000-0000-0000-000000000000) will never be equal to NEWID() because NEWID() complies with RFC4122 which should always include the version number in the generated UNIQUEIDENTIFIER which can't be 0 because the version number of random UUIDs is 4.
+    -- See more:
+    -- https://learn.microsoft.com/en-us/sql/t-sql/functions/newid-transact-sql
+    -- https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.3
     CAST('00000000-0000-0000-0000-000000000000' AS UNIQUEIDENTIFIER) AS NULLISH_UNIQUEIDENTIFIER,
     CAST('' AS NVARCHAR) AS NULLISH_NVARCHAR
