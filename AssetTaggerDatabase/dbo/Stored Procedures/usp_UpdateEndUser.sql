2d34f46211ac033134ef7d109455421cdb91d85c
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-08 18:52:09 +0800

    Update CRU stored procedures of EndUser to handle change of foreign UNIQUEIDENTIFIERs not being nullable

--- AssetTaggerDatabase/dbo/Stored Procedures/usp_UpdateEndUser.sql
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_UpdateEndUser.sql
@@ -2,8 +2,8 @@
     @CallingEndUserID UNIQUEIDENTIFIER,
     @EndUserID UNIQUEIDENTIFIER,
     @EndUserName NVARCHAR(50) = NULL,
-    @EndUserRoleID UNIQUEIDENTIFIER = '00000000-0000-0000-0000-000000000000',
-    @EmployeeID UNIQUEIDENTIFIER = '00000000-0000-0000-0000-000000000000'
+    @EndUserRoleID UNIQUEIDENTIFIER = NULL,
+    @EmployeeID UNIQUEIDENTIFIER = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -23,12 +23,8 @@ BEGIN
             RETURN -1;
         END
 
-    -- Get CONSTANTS.
-    DECLARE @NULLISH_UNIQUEIDENTIFIER UNIQUEIDENTIFIER;
-    SELECT @NULLISH_UNIQUEIDENTIFIER = (SELECT NULLISH_UNIQUEIDENTIFIER FROM [dbo].[VI_NullishConstants]);
-
     -- Validate input.
-    IF (@EndUserName IS NULL AND @EndUserRoleID = @NULLISH_UNIQUEIDENTIFIER AND @EmployeeID = @NULLISH_UNIQUEIDENTIFIER)
+    IF (@EndUserName IS NULL AND @EndUserRoleID IS NULL AND @EmployeeID IS NULL)
         BEGIN
             RAISERROR ('Cannot update row with @EndUserID when no non-default values are passed to other parameters!', 11, 0);
             RETURN -1;
@@ -36,7 +32,7 @@ BEGIN
 
     -- Run actual query.
     UPDATE [dbo].[EndUser]
-    SET EndUserName = ISNULL(@EndUserName, EndUserName), EndUserRoleID = IIF(@EndUserRoleID = @NULLISH_UNIQUEIDENTIFIER, EndUserRoleID, @EndUserRoleID), EmployeeID = IIF(@EmployeeID = @NULLISH_UNIQUEIDENTIFIER, EmployeeID, @EmployeeID)
+    SET EndUserName = ISNULL(@EndUserName, EndUserName), EndUserRoleID = ISNULL(@EndUserRoleID, EndUserRoleID), EmployeeID = ISNULL(@EmployeeID, EmployeeID)
     OUTPUT INSERTED.EndUserID, INSERTED.EndUserName, INSERTED.EndUserRoleID, INSERTED.EmployeeID, DELETED.EndUserName AS OldEndUserName, DELETED.EndUserRoleID AS OldEndUserRoleID, DELETED.EmployeeID AS OldEmployeeID
     FROM [dbo].[EndUser]
     WHERE EndUserID = @EndUserID;
