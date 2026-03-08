d72b3b9a03e37a6a9b50febe3d3f53c9c6ff31c7
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-08 18:28:47 +0800

    Delete CK_EndUser_EndUserName_OnlyLowercase constraint because the default collation for the locale 'English (United States)' is 'SQL_Latin1_General_CP1_CI_AS' which is case-insensitive (CI)
    See more:
    https://learn.microsoft.com/en-us/sql/relational-databases/collations/collation-and-unicode-support#collation

--- AssetTaggerDatabase/dbo/Tables/EndUser.sql
+++ AssetTaggerDatabase/dbo/Tables/EndUser.sql
@@ -8,7 +8,6 @@
     CONSTRAINT [AK_EndUser_EmployeeID] UNIQUE ([EmployeeID]),
     CONSTRAINT [CK_EndUser_EndUserName_MinimumLength] CHECK (LEN([EndUserName]) > 0),
     CONSTRAINT [CK_EndUser_EndUserName_NoWhitespace] CHECK (CHARINDEX(' ', [EndUserName]) = 0),
-    CONSTRAINT [CK_EndUser_EndUserName_OnlyLowercase] CHECK (BINARY_CHECKSUM([EndUserName]) = BINARY_CHECKSUM(LOWER([EndUserName]))),
     CONSTRAINT [PK_EndUser] PRIMARY KEY CLUSTERED ([EndUserID] ASC),
     CONSTRAINT [FK_EndUser_EndUserRole] FOREIGN KEY ([EndUserRoleID]) REFERENCES [dbo].[EndUserRole] ([EndUserRoleID]),
     CONSTRAINT [FK_EndUser_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID])
