746c8257905e889510c416832eda2af5e059cd31
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-08 17:25:24 +0800

    Add constraints to not allow trailing spaces for NVARCHAR columns

--- AssetTaggerDatabase/dbo/Tables/AssetIssue.sql
+++ AssetTaggerDatabase/dbo/Tables/AssetIssue.sql
@@ -7,6 +7,7 @@
     [EmployeeID] UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_AssetIssue] PRIMARY KEY CLUSTERED ([AssetIssueID] ASC),
     CONSTRAINT [CK_AssetIssue_AssetIssueTitle_NoTrailingSpace] CHECK ([AssetIssueTitle] NOT LIKE ' %' AND [AssetIssueTitle] NOT LIKE '% '),
+    CONSTRAINT [CK_AssetIssue_AssetIssueDesc_NoTrailingSpace] CHECK ([AssetIssueDesc] NOT LIKE ' %' AND [AssetIssueDesc] NOT LIKE '% '),
     CONSTRAINT [FK_AssetIssue_Asset] FOREIGN KEY ([AssetID]) REFERENCES [dbo].[Asset] ([AssetID]),
     CONSTRAINT [FK_AssetIssue_Employee] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employee] ([EmployeeID])
 );
