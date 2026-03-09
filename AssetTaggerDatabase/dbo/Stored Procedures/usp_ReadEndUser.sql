645fd778789c01647650bbe9caeb43f2aa564ac4
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-09 16:03:40 +0800

    Implement pagination

--- AssetTaggerDatabase/dbo/Stored Procedures/usp_ReadEndUser.sql
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_ReadEndUser.sql
@@ -3,7 +3,9 @@
     @EndUserID UNIQUEIDENTIFIER = NULL,
     @EndUserName NVARCHAR(4000) = NULL,
     @EndUserRoleID UNIQUEIDENTIFIER = NULL,
-    @EmployeeID UNIQUEIDENTIFIER = NULL
+    @EmployeeID UNIQUEIDENTIFIER = NULL,
+    @RowsToSkip INT = NULL,
+    @RowsToReturn INT = NULL
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -37,5 +39,12 @@ BEGIN
         EndUserRoleID,
         EmployeeID
     FROM [dbo].[EndUser]
-    WHERE EndUserID = ISNULL(@EndUserID, EndUserID) AND EndUserName = ISNULL(@EndUserName, EndUserName) AND EndUserRoleID = ISNULL(@EndUserRoleID, EndUserRoleID) AND EmployeeID = ISNULL(@EmployeeID, EmployeeID);
+    WHERE EndUserID = ISNULL(@EndUserID, EndUserID) AND EndUserName = ISNULL(@EndUserName, EndUserName) AND EndUserRoleID = ISNULL(@EndUserRoleID, EndUserRoleID) AND EmployeeID = ISNULL(@EmployeeID, EmployeeID)
+    ORDER BY
+        EndUserNumber ASC
+        OFFSET ISNULL(@RowsToSkip, 0) ROWS
+        -- If @RowsToReturn is NULL fetch the next 2,147,483,647 rows which is the upper limit of INT.
+        -- See more:
+        -- https://learn.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql
+        FETCH NEXT ISNULL(@RowsToReturn, 2147483647) ROWS ONLY;
 END
