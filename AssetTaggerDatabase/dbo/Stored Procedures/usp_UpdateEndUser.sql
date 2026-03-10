6fad06b665f019cd8b361e2a2f19059fedd0959e
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-10 09:30:29 +0800

    Include EndUserRegisterDate in returned row

--- AssetTaggerDatabase/dbo/Stored Procedures/usp_UpdateEndUser.sql
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_UpdateEndUser.sql
@@ -33,7 +33,7 @@ BEGIN
     -- Run actual query.
     UPDATE [dbo].[EndUser]
     SET EndUserName = ISNULL(@EndUserName, EndUserName), EndUserRoleID = ISNULL(@EndUserRoleID, EndUserRoleID), EmployeeID = ISNULL(@EmployeeID, EmployeeID)
-    OUTPUT INSERTED.EndUserID, INSERTED.EndUserName, INSERTED.EndUserRoleID, INSERTED.EmployeeID, DELETED.EndUserName AS OldEndUserName, DELETED.EndUserRoleID AS OldEndUserRoleID, DELETED.EmployeeID AS OldEmployeeID
+    OUTPUT INSERTED.EndUserID, INSERTED.EndUserName, INSERTED.EndUserRoleID, INSERTED.EmployeeID, INSERTED.EndUserRegisterDate, DELETED.EndUserName AS OldEndUserName, DELETED.EndUserRoleID AS OldEndUserRoleID, DELETED.EmployeeID AS OldEmployeeID
     FROM [dbo].[EndUser]
     WHERE EndUserID = @EndUserID;
 END
