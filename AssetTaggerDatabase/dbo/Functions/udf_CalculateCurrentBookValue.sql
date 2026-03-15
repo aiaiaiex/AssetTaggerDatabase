a8c5b6fbe955a8200a626f1ffcd6d332dcb4d2a5
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-16 01:41:11 +0800

    Create udf_CalculateCurrentBookValue

---
+++ AssetTaggerDatabase/dbo/Functions/udf_CalculateCurrentBookValue.sql
@@ -0,0 +1,20 @@
+CREATE FUNCTION [dbo].[udf_CalculateCurrentBookValue](
+    @AssetPurchasePrice DECIMAL(19, 4),
+    @AssetSalvageValue DECIMAL(19, 4),
+    @AssetUsefulLife INT,
+    @AssetPurchaseDate DATETIME
+)
+RETURNS DECIMAL(19, 4) AS
+BEGIN
+    DECLARE @YearsPassed INT = DATEDIFF(DD, @AssetPurchaseDate, GETDATE()) / 365;
+
+    IF (@YearsPassed < 0)
+        RETURN NULL
+
+    DECLARE @CurrentBookValue DECIMAL(19, 4) = @AssetPurchasePrice - [dbo].[udf_CalculateAnnualDepreciationExpense](@AssetPurchasePrice, @AssetSalvageValue, @AssetUsefulLife) * @YearsPassed;
+
+    IF (@CurrentBookValue < @AssetSalvageValue)
+        RETURN @AssetSalvageValue
+
+    RETURN @CurrentBookValue;
+END;
