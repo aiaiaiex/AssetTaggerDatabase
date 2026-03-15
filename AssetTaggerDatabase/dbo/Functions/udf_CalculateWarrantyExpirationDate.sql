c80bad805cff34e4564778d76a52c99135ecb661
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-16 01:57:13 +0800

    Create udf_CalculateWarrantyExpirationDate

---
+++ AssetTaggerDatabase/dbo/Functions/udf_CalculateWarrantyExpirationDate.sql
@@ -0,0 +1,20 @@
+CREATE FUNCTION [dbo].[udf_CalculateWarrantyExpirationDate](
+    @AssetWarrantyUnitOfMeasure NCHAR(2),
+    @AssetWarrantyDuration INT,
+    @AssetPurchaseDate DATETIME
+)
+RETURNS DATETIME AS
+BEGIN
+    IF (@AssetWarrantyDuration < 0)
+        RETURN NULL
+
+    IF (@AssetWarrantyDuration = 0)
+        RETURN @AssetPurchaseDate;
+
+    RETURN CASE
+        WHEN @AssetWarrantyUnitOfMeasure = 'yy' THEN DATEADD(YY, @AssetWarrantyDuration, @AssetPurchaseDate)
+        WHEN @AssetWarrantyUnitOfMeasure = 'mm' THEN DATEADD(MM, @AssetWarrantyDuration, @AssetPurchaseDate)
+        WHEN @AssetWarrantyUnitOfMeasure = 'ww' THEN DATEADD(WW, @AssetWarrantyDuration, @AssetPurchaseDate)
+        WHEN @AssetWarrantyUnitOfMeasure = 'dd' THEN DATEADD(DD, @AssetWarrantyDuration, @AssetPurchaseDate)
+    END;
+END;
