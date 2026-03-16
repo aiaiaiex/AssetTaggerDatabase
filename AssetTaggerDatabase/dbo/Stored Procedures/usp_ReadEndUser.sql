ad331ebfcb2382947504d4c7807b39a4e3018601
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-16 19:44:08 +0800

    Add more to comment

--- AssetTaggerDatabase/dbo/Stored Procedures/usp_ReadEndUser.sql
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_ReadEndUser.sql
@@ -70,7 +70,7 @@ BEGIN
     ORDER BY
         EndUserNumber ASC
         OFFSET ISNULL(@RowsToSkip, 0) ROWS
-        -- If @RowsToReturn is NULL fetch the next 2,147,483,647 rows which is the upper limit of INT.
+        -- If @RowsToReturn is NULL fetch the next 2,147,483,647 rows which is the upper limit of INT, the data type of EndUserNumber.
         -- See more:
         -- https://learn.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql
         FETCH NEXT ISNULL(@RowsToReturn, 2147483647) ROWS ONLY;
