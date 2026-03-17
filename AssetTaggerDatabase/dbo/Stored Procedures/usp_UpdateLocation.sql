4bd54622c6f9a5da5d42fab4427518ea3b89638a
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 05:25:45 +0800

    Create usp_UpdateLocation

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_UpdateLocation.sql
@@ -0,0 +1,41 @@
+CREATE PROCEDURE [dbo].[usp_UpdateLocation]
+    @CallingEndUserID UNIQUEIDENTIFIER,
+    @LocationID UNIQUEIDENTIFIER,
+    @LocationAddress NVARCHAR(842) = NULL,
+    @BuildingID UNIQUEIDENTIFIER = NULL
+AS;
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check updating permission of the calling EndUser.
+    DECLARE @UpdateLocation BIT = (SELECT UpdateLocation FROM [dbo].[tvf_GetCRUDPermissionsOfEndUser](@CallingEndUserID));
+
+    IF (@UpdateLocation IS NULL)
+        BEGIN
+            RAISERROR ('@CallingEndUserID does not exist!', 11, 0);
+            RETURN -1;
+        END;
+    IF (@UpdateLocation = 0)
+        BEGIN
+            RAISERROR ('@CallingEndUserID has no permission to update a Location!', 11, 0);
+            RETURN -1;
+        END;
+
+    -- Run actual query.
+    UPDATE
+        [dbo].[Location]
+    SET
+        LocationAddress = ISNULL(@LocationAddress, LocationAddress),
+        BuildingID = ISNULL(@BuildingID, BuildingID)
+    OUTPUT
+        INSERTED.LocationID,
+        INSERTED.LocationAddress,
+        INSERTED.BuildingID,
+        INSERTED.LocationInsertDate,
+        DELETED.LocationAddress AS OldLocationAddress,
+        DELETED.BuildingID AS OldBuildingID
+    FROM
+        [dbo].[Location]
+    WHERE
+        LocationID = @LocationID;
+END;
