ff5c5a793dc64b69b9deb9c4994f72e4cdcdc63a
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 00:16:42 +0800

    Create usp_CreateVendor

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_CreateVendor.sql
@@ -0,0 +1,27 @@
+CREATE PROCEDURE [dbo].[usp_CreateVendor]
+    @CallingEndUserID UNIQUEIDENTIFIER,
+    @VendorName NVARCHAR(850),
+    @VendorAddress NVARCHAR(850)
+AS;
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check creating permission of the calling EndUser.
+    DECLARE @CreateVendor BIT = (SELECT CreateVendor FROM [dbo].[tvf_GetCRUDPermissionsOfEndUser](@CallingEndUserID));
+
+    IF (@CreateVendor IS NULL)
+        BEGIN
+            RAISERROR ('@CallingEndUserID does not exist!', 11, 0);
+            RETURN -1;
+        END
+    IF (@CreateVendor = 0)
+        BEGIN
+            RAISERROR ('@CallingEndUserID has no permission to create a Vendor!', 11, 0);
+            RETURN -1;
+        END
+
+    -- Run actual query.
+    INSERT INTO [dbo].[Vendor] (VendorName, VendorAddress)
+    OUTPUT INSERTED.VendorID, INSERTED.VendorName, INSERTED.VendorAddress, INSERTED.VendorInsertDate
+    VALUES (@VendorName, @VendorAddress);
+END;
