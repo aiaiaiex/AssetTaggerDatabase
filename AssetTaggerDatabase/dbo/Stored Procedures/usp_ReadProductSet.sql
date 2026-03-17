c51c5f2bfe6604c1f5a76de924c28223f9c3f7a7
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 06:19:37 +0800

    Create usp_ReadProductSet

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_ReadProductSet.sql
@@ -0,0 +1,53 @@
+CREATE PROCEDURE [dbo].[usp_ReadProductSet]
+    @CallingEndUserID UNIQUEIDENTIFIER,
+    @ParentProductID UNIQUEIDENTIFIER = NULL,
+    @ProductID UNIQUEIDENTIFIER = NULL,
+    @FromProductSetProductQuantity INT = NULL,
+    @ToProductSetProductQuantity INT = NULL,
+    @FromProductSetInsertDate DATETIME = NULL,
+    @ToProductSetInsertDate DATETIME = NULL,
+    @RowsToSkip INT = NULL,
+    @RowsToReturn INT = NULL,
+    @NewestRowsFirst BIT = 1
+AS;
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check reading permission of the calling EndUser.
+    DECLARE @ReadProductSet BIT = (SELECT ReadProductSet FROM [dbo].[tvf_GetCRUDPermissionsOfEndUser](@CallingEndUserID));
+
+    IF (@ReadProductSet IS NULL)
+        BEGIN
+            RAISERROR ('@CallingEndUserID does not exist!', 11, 0);
+            RETURN -1;
+        END;
+    IF (@ReadProductSet = 0)
+        BEGIN
+            RAISERROR ('@CallingEndUserID has no permission to read ProductSet!', 11, 0);
+            RETURN -1;
+        END;
+
+    -- Run actual query.
+    SELECT
+        ParentProductID,
+        ProductID,
+        ProductSetProductQuantity,
+        ProductSetInsertDate
+    FROM
+        [dbo].[ProductSet]
+    WHERE
+        ParentProductID = ISNULL(@ParentProductID, ParentProductID)
+        AND ProductID = ISNULL(@ProductID, ProductID)
+        AND ISNULL(@FromProductSetProductQuantity, ProductSetProductQuantity) <= ProductSetProductQuantity
+        AND ProductSetProductQuantity <= ISNULL(@ToProductSetProductQuantity, ProductSetProductQuantity)
+        AND ISNULL(@FromProductSetInsertDate, ProductSetInsertDate) <= ProductSetInsertDate
+        AND ProductSetInsertDate <= ISNULL(@ToProductSetInsertDate, ProductSetInsertDate)
+    ORDER BY
+        CASE WHEN @NewestRowsFirst = 1 THEN ProductSetNumber END DESC,
+        CASE WHEN @NewestRowsFirst = 0 THEN ProductSetNumber END ASC
+        OFFSET ISNULL(@RowsToSkip, 0) ROWS
+        -- If @RowsToReturn is NULL fetch the next 2,147,483,647 rows which is the upper limit of INT, the data type of EndUserNumber.
+        -- See more:
+        -- https://learn.microsoft.com/en-us/sql/t-sql/data-types/int-bigint-smallint-and-tinyint-transact-sql
+        FETCH NEXT ISNULL(@RowsToReturn, 2147483647) ROWS ONLY;
+END;
