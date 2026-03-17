6caa8a40f03f446a0c84e760e3cdba640bd958d1
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 00:21:33 +0800

    Create usp_DeleteVendor

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_DeleteVendor.sql
@@ -0,0 +1,27 @@
+CREATE PROCEDURE [dbo].[usp_DeleteVendor]
+    @CallingEndUserID UNIQUEIDENTIFIER,
+    @VendorID UNIQUEIDENTIFIER
+AS;
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check deleting permission of the calling EndUser.
+    DECLARE @DeleteVendor BIT = (SELECT DeleteVendor FROM [dbo].[tvf_GetCRUDPermissionsOfEndUser](@CallingEndUserID));
+
+    IF (@DeleteVendor IS NULL)
+        BEGIN
+            RAISERROR ('@CallingEndUserID does not exist!', 11, 0);
+            RETURN -1;
+        END;
+    IF (@DeleteVendor = 0)
+        BEGIN
+            RAISERROR ('@CallingEndUserID has no permission to delete a Vendor!', 11, 0);
+            RETURN -1;
+        END;
+
+    -- Run actual query.
+    DELETE [dbo].[Vendor]
+    OUTPUT DELETED.VendorID, DELETED.VendorName, DELETED.VendorAddress, DELETED.VendorInsertDate
+    FROM [dbo].[Vendor]
+    WHERE VendorID = @VendorID;
+END;
