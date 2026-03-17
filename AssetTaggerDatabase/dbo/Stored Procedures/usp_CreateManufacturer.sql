69a3b6f0c4fd642431d9e47e2ff874e3f0691736
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 00:50:53 +0800

    Create usp_CreateManufacturer

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_CreateManufacturer.sql
@@ -0,0 +1,26 @@
+CREATE PROCEDURE [dbo].[usp_CreateManufacturer]
+    @CallingEndUserID UNIQUEIDENTIFIER,
+    @ManufacturerName NVARCHAR(850)
+AS;
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check creating permission of the calling EndUser.
+    DECLARE @CreateManufacturer BIT = (SELECT CreateManufacturer FROM [dbo].[tvf_GetCRUDPermissionsOfEndUser](@CallingEndUserID));
+
+    IF (@CreateManufacturer IS NULL)
+        BEGIN
+            RAISERROR ('@CallingEndUserID does not exist!', 11, 0);
+            RETURN -1;
+        END
+    IF (@CreateManufacturer = 0)
+        BEGIN
+            RAISERROR ('@CallingEndUserID has no permission to create a Manufacturer!', 11, 0);
+            RETURN -1;
+        END
+
+    -- Run actual query.
+    INSERT INTO [dbo].[Manufacturer] (ManufacturerName)
+    OUTPUT INSERTED.ManufacturerID, INSERTED.ManufacturerName, INSERTED.ManufacturerInsertDate
+    VALUES (@ManufacturerName);
+END;
