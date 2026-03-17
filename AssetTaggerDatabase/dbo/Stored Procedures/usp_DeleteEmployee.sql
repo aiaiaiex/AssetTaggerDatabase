fccf25bd445c7d36fa8d6b86bcff8c79f63bfe6f
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 04:37:26 +0800

    Create usp_DeleteEmployee

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_DeleteEmployee.sql
@@ -0,0 +1,35 @@
+CREATE PROCEDURE [dbo].[usp_DeleteEmployee]
+    @CallingEndUserID UNIQUEIDENTIFIER,
+    @EmployeeID UNIQUEIDENTIFIER
+AS;
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check deleting permission of the calling EndUser.
+    DECLARE @DeleteEmployee BIT = (SELECT DeleteEmployee FROM [dbo].[tvf_GetCRUDPermissionsOfEndUser](@CallingEndUserID));
+
+    IF (@DeleteEmployee IS NULL)
+        BEGIN
+            RAISERROR ('@CallingEndUserID does not exist!', 11, 0);
+            RETURN -1;
+        END;
+    IF (@DeleteEmployee = 0)
+        BEGIN
+            RAISERROR ('@CallingEndUserID has no permission to delete an Employee!', 11, 0);
+            RETURN -1;
+        END;
+
+    -- Run actual query.
+    DELETE [dbo].[Employee]
+    OUTPUT
+        DELETED.EmployeeID,
+        DELETED.EmployeeFullName,
+        DELETED.RoleID,
+        DELETED.CompanyID,
+        DELETED.DepartmentID,
+        DELETED.EmployeeInsertDate
+    FROM
+        [dbo].[Employee]
+    WHERE
+        EmployeeID = @EmployeeID;
+END;
