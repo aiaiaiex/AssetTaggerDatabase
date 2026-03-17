1739ecfde829ffb8df2bec33839a8d900bd9bd7b
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 03:43:20 +0800

    Create usp_UpdateCategory

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_UpdateCategory.sql
@@ -0,0 +1,37 @@
+CREATE PROCEDURE [dbo].[usp_UpdateCategory]
+    @CallingEndUserID UNIQUEIDENTIFIER,
+    @CategoryID UNIQUEIDENTIFIER,
+    @CategoryName NVARCHAR(850) = NULL
+AS;
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check updating permission of the calling EndUser.
+    DECLARE @UpdateCategory BIT = (SELECT UpdateCategory FROM [dbo].[tvf_GetCRUDPermissionsOfEndUser](@CallingEndUserID));
+
+    IF (@UpdateCategory IS NULL)
+        BEGIN
+            RAISERROR ('@CallingEndUserID does not exist!', 11, 0);
+            RETURN -1;
+        END;
+    IF (@UpdateCategory = 0)
+        BEGIN
+            RAISERROR ('@CallingEndUserID has no permission to update a Category!', 11, 0);
+            RETURN -1;
+        END;
+
+    -- Run actual query.
+    UPDATE
+        [dbo].[Category]
+    SET
+        CategoryName = ISNULL(@CategoryName, CategoryName)
+    OUTPUT
+        INSERTED.CategoryID,
+        INSERTED.CategoryName,
+        INSERTED.CategoryInsertDate,
+        DELETED.CategoryName AS OldCategoryName
+    FROM
+        [dbo].[Category]
+    WHERE
+        CategoryID = @CategoryID;
+END;
