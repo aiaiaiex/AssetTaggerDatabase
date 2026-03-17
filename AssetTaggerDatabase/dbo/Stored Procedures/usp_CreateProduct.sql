f7f674e59a35022b6cb67023fd9b4d5c682d83b4
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-18 03:55:17 +0800

    Create usp_CreateProduct

---
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_CreateProduct.sql
@@ -0,0 +1,49 @@
+CREATE PROCEDURE [dbo].[usp_CreateProduct]
+    @CallingEndUserID UNIQUEIDENTIFIER,
+    @ProductName NVARCHAR(421) = NULL,
+    @ProductModelNumber NVARCHAR(421) = NULL,
+    @ProductDocumentationURL NVARCHAR(4000) = NULL,
+    @ManufacturerID UNIQUEIDENTIFIER = NULL,
+    @CategoryID UNIQUEIDENTIFIER
+AS;
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Check creating permission of the calling EndUser.
+    DECLARE @CreateProduct BIT = (SELECT CreateProduct FROM [dbo].[tvf_GetCRUDPermissionsOfEndUser](@CallingEndUserID));
+
+    IF (@CreateProduct IS NULL)
+        BEGIN
+            RAISERROR ('@CallingEndUserID does not exist!', 11, 0);
+            RETURN -1;
+        END;
+    IF (@CreateProduct = 0)
+        BEGIN
+            RAISERROR ('@CallingEndUserID has no permission to create a Product!', 11, 0);
+            RETURN -1;
+        END;
+
+    -- Run actual query.
+    INSERT INTO [dbo].[Product] (
+        ProductName,
+        ProductModelNumber,
+        ProductDocumentationURL,
+        ManufacturerID,
+        CategoryID
+    )
+    OUTPUT
+        INSERTED.ProductID,
+        INSERTED.ProductName,
+        INSERTED.ProductModelNumber,
+        INSERTED.ProductDocumentationURL,
+        INSERTED.ManufacturerID,
+        INSERTED.CategoryID,
+        INSERTED.ProductInsertDate
+    VALUES (
+        @ProductName,
+        @ProductModelNumber,
+        @ProductDocumentationURL,
+        @ManufacturerID,
+        @CategoryID
+    );
+END;
