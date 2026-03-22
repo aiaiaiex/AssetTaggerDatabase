4de1423ac8f5c288369b6c5fd98621989637c1b0
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-22 13:21:17 +0800

    Change condition for matching nullable DATETIMEOFFSET(3) columns
    Because previously if a value given to a nullable DATETIMEOFFSET(3) parameter is not @NON_NULLISH_DATETIMEOFFSET, it will match to NULL DATETIMEOFFSET(3) column even if the given value isn't NULL or @NULLISH_DATETIMEOFFSET.

--- AssetTaggerDatabase/dbo/Stored Procedures/usp_ReadAssetFix.sql
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_ReadAssetFix.sql
@@ -67,8 +67,8 @@ BEGIN
         AND AssetFixed = ISNULL(@AssetFixed, AssetFixed)
         AND ISNULL(@FromAssetFixDateStart, AssetFixDateStart) <= AssetFixDateStart
         AND AssetFixDateStart <= ISNULL(@ToAssetFixDateStart, AssetFixDateStart)
-        AND (IIF(@FromAssetFixDateEnd IN (@NULLISH_DATETIMEOFFSET, @NON_NULLISH_DATETIMEOFFSET), AssetFixDateEnd, @FromAssetFixDateEnd) <= AssetFixDateEnd OR AssetFixDateEnd IS NOT DISTINCT FROM IIF(@FromAssetFixDateEnd = @NON_NULLISH_DATETIMEOFFSET, @NON_NULLISH_DATETIMEOFFSET, NULL))
-        AND (AssetFixDateEnd <= IIF(@ToAssetFixDateEnd IN (@NULLISH_DATETIMEOFFSET, @NON_NULLISH_DATETIMEOFFSET), AssetFixDateEnd, @ToAssetFixDateEnd) OR AssetFixDateEnd IS NOT DISTINCT FROM IIF(@ToAssetFixDateEnd = @NON_NULLISH_DATETIMEOFFSET, @NON_NULLISH_DATETIMEOFFSET, NULL))
+        AND (IIF(@FromAssetFixDateEnd IN (@NULLISH_DATETIMEOFFSET, @NON_NULLISH_DATETIMEOFFSET), AssetFixDateEnd, @FromAssetFixDateEnd) <= AssetFixDateEnd OR AssetFixDateEnd IS NOT DISTINCT FROM IIF(@FromAssetFixDateEnd = @NULLISH_DATETIMEOFFSET OR @FromAssetFixDateEnd IS NULL, NULL, @NON_NULLISH_DATETIMEOFFSET))
+        AND (AssetFixDateEnd <= IIF(@ToAssetFixDateEnd IN (@NULLISH_DATETIMEOFFSET, @NON_NULLISH_DATETIMEOFFSET), AssetFixDateEnd, @ToAssetFixDateEnd) OR AssetFixDateEnd IS NOT DISTINCT FROM IIF(@ToAssetFixDateEnd = @NULLISH_DATETIMEOFFSET OR @ToAssetFixDateEnd IS NULL, NULL, @NON_NULLISH_DATETIMEOFFSET))
         AND (IIF(@FromAssetFixCost IN (@NULLISH_DECIMAL, @NON_NULLISH_DECIMAL), AssetFixCost, @FromAssetFixCost) <= AssetFixCost OR AssetFixCost IS NOT DISTINCT FROM IIF(@FromAssetFixCost = @NON_NULLISH_DECIMAL, @NON_NULLISH_DECIMAL, NULL))
         AND (AssetFixCost <= IIF(@ToAssetFixCost IN (@NULLISH_DECIMAL, @NON_NULLISH_DECIMAL), AssetFixCost, @ToAssetFixCost) OR AssetFixCost IS NOT DISTINCT FROM IIF(@ToAssetFixCost = @NON_NULLISH_DECIMAL, @NON_NULLISH_DECIMAL, NULL))
     ORDER BY
