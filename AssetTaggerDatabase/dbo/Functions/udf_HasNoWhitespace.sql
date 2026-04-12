d67e52bf66f1e87b895c8a5e2e3d1a4316de2041
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-13 06:08:39 +0800

    Create udf_HasNoWhitespace

---
+++ AssetTaggerDatabase/dbo/Functions/udf_HasNoWhitespace.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION [dbo].[udf_HasNoWhitespace](
+    @Value NVARCHAR(4000)
+)
+RETURNS BIT WITH SCHEMABINDING AS
+BEGIN
+    RETURN CASE
+        WHEN (CHARINDEX(' ', @Value) = 0)
+            THEN 1
+        ELSE 0
+    END;
+END;
