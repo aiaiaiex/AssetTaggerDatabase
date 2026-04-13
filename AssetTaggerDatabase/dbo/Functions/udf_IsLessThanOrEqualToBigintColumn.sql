fe6d69c647d6454c0d8c6a28a2ca1fbbea2cf3f3
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-14 02:27:29 +0800

    Create user-defined functions for comparing BIGINT values

---
+++ AssetTaggerDatabase/dbo/Functions/udf_IsLessThanOrEqualToBigintColumn.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION [dbo].[udf_IsLessThanOrEqualToBigintColumn](
+    @Value NVARCHAR(20),
+    @ColumnValue BIGINT
+)
+RETURNS BIT WITH SCHEMABINDING AS
+BEGIN
+    RETURN CASE
+        WHEN (
+            (@Value = '')
+            OR (CAST(@Value AS BIGINT) <= @ColumnValue)
+            OR (@Value IS NULL AND @ColumnValue IS NULL)
+        )
+            THEN 1
+        ELSE 0
+    END;
+END;
