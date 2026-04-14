e8690a45490e5c90b78bf3af255e7d0092e33b5e
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-15 05:08:30 +0800

    Refactor usp_CreateAuthentication

--- AssetTaggerDatabase/dbo/Stored Procedures/usp_CreateAuthentication.sql
+++ AssetTaggerDatabase/dbo/Stored Procedures/usp_CreateAuthentication.sql
@@ -1,14 +1,19 @@
 ﻿CREATE PROCEDURE [dbo].[usp_CreateAuthentication]
-    @EndUserUsername NVARCHAR(4000),
-    @EndUserPassword NVARCHAR(MAX)
+    -- Non-nullable columns.
+    @EndUserUsername NVARCHAR(4000) = '',
+    -- Secret parameters.
+    @EndUserPassword NVARCHAR(MAX) = ''
 AS;
 BEGIN
     SET NOCOUNT ON;
 
+    -- Non-nullable columns with default values.
     SELECT Id
     FROM
         [dbo].[EndUser]
     WHERE
-        Username = @EndUserUsername
-        AND PasswordHash = [dbo].[udf_HashPassword](CONCAT(@EndUserPassword, CAST(PasswordSalt AS NVARCHAR(36))));
+        -- Non-nullable columns.
+        Username = [dbo].[udf_GetDefaultNvarchar](@EndUserUsername, NULL)
+        -- Secret columns.
+        AND PasswordHash = [dbo].[udf_HashPassword](CONCAT([dbo].[udf_GetDefaultNvarcharMax](@EndUserPassword, NULL), CAST(PasswordSalt AS NVARCHAR(36))));
 END;
