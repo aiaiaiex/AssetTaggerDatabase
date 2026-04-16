7ada298e082ce181e975ea6e74454c811b788c99
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-16 22:09:08 +0800

    Create a composite index

--- AssetTaggerDatabase/dbo/Tables/Vendor.sql
+++ AssetTaggerDatabase/dbo/Tables/Vendor.sql
@@ -10,7 +10,6 @@
 
     -- Non-nullable columns.
     [Name] NVARCHAR(850) NOT NULL,
-    CONSTRAINT [AK_Vendor_Name] UNIQUE ([Name]),
     CONSTRAINT [CK_Vendor_Name_IsNotReservedKeyword] CHECK ([dbo].[udf_IsNotReservedKeywordInNvarchar]([Name]) = 1),
     CONSTRAINT [CK_Vendor_Name_HasNoLeadingAndTrailingWhitespace] CHECK ([dbo].[udf_HasNoLeadingAndTrailingWhitespaceInNvarchar]([Name]) = 1),
 
@@ -20,6 +19,5 @@
     CONSTRAINT [CK_Vendor_Address_HasNoLeadingAndTrailingWhitespace] CHECK ([dbo].[udf_HasNoLeadingAndTrailingWhitespaceInNvarchar]([Address]) = 1)
 );
 
-CREATE UNIQUE NONCLUSTERED INDEX [IX_Vendor_Address]
-    ON [dbo].[Asset] ([Address])
-    WHERE [Address] IS NOT NULL;
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Vendor_Name_Address]
+    ON [dbo].[Asset] ([Name], [Address]);
