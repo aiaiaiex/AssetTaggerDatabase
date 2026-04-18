3180a77676786e1e776138e690344e478a8c679c
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-18 20:58:03 +0800

    Change NVARCHAR length to 4000 since Address isn't unique anymore
    And make Address nullable which should have been done on this file's previous commit

--- AssetTaggerDatabase/dbo/Tables/Building.sql
+++ AssetTaggerDatabase/dbo/Tables/Building.sql
@@ -19,7 +19,7 @@
     CONSTRAINT [CK_Building_Name_HasNoLeadingAndTrailingWhitespace] CHECK ([dbo].[udf_HasNoLeadingAndTrailingWhitespaceInNvarchar]([Name]) = 1),
 
     -- Nullable columns.
-    [Address] NVARCHAR(850) NOT NULL,
+    [Address] NVARCHAR(4000) NULL,
     CONSTRAINT [CK_Building_Address_IsNotReservedKeyword] CHECK ([dbo].[udf_IsNotReservedKeywordInNvarchar]([Address]) = 1),
     CONSTRAINT [CK_Building_Address_HasNoLeadingAndTrailingWhitespace] CHECK ([dbo].[udf_HasNoLeadingAndTrailingWhitespaceInNvarchar]([Address]) = 1)
 );
