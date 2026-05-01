6d93d20eacdf6392dfc4dd75bfa721256e604ba5
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-05-01 19:15:34 +0800

    Include new tables
    And delete them in alphabetical order

--- AssetTaggerDatabase/dbo/Utils/DeleteAllRows.sql
+++ AssetTaggerDatabase/dbo/Utils/DeleteAllRows.sql
@@ -4,19 +4,21 @@ DELETE FROM [dbo].[Asset];
 DELETE FROM [dbo].[Log];
 -- Has foreign keys from tables below.
 DELETE FROM [dbo].[EndUser];
+DELETE FROM [dbo].[InHouseUnit];
 DELETE FROM [dbo].[Location];
 DELETE FROM [dbo].[ProductSet];
 -- Has foreign keys from tables below.
 DELETE FROM [dbo].[Building];
 DELETE FROM [dbo].[Employee];
+DELETE FROM [dbo].[Permission];
 DELETE FROM [dbo].[Product];
 -- Has no foreign keys.
 DELETE FROM [dbo].[Category];
 DELETE FROM [dbo].[Company];
 DELETE FROM [dbo].[Department];
-DELETE FROM [dbo].[Role];
-DELETE FROM [dbo].[Manufacturer];
 DELETE FROM [dbo].[Job];
+DELETE FROM [dbo].[Manufacturer];
+DELETE FROM [dbo].[Role];
 DELETE FROM [dbo].[Vendor];
 
 -- Reset identity values back to 0.
@@ -25,17 +27,19 @@ DBCC CHECKIDENT('dbo.Asset', RESEED, 0);
 DBCC CHECKIDENT('dbo.Log', RESEED, 0);
 -- Has foreign keys from tables below.
 DBCC CHECKIDENT('dbo.EndUser', RESEED, 0);
+DBCC CHECKIDENT('dbo.InHouseUnit', RESEED, 0);
 DBCC CHECKIDENT('dbo.Location', RESEED, 0);
 DBCC CHECKIDENT('dbo.ProductSet', RESEED, 0);
 -- Has foreign keys from tables below.
 DBCC CHECKIDENT('dbo.Building', RESEED, 0);
 DBCC CHECKIDENT('dbo.Employee', RESEED, 0);
+DBCC CHECKIDENT('dbo.Permission', RESEED, 0);
 DBCC CHECKIDENT('dbo.Product', RESEED, 0);
 -- Has no foreign keys.
 DBCC CHECKIDENT('dbo.Category', RESEED, 0);
 DBCC CHECKIDENT('dbo.Company', RESEED, 0);
 DBCC CHECKIDENT('dbo.Department', RESEED, 0);
-DBCC CHECKIDENT('dbo.Role', RESEED, 0);
-DBCC CHECKIDENT('dbo.Manufacturer', RESEED, 0);
 DBCC CHECKIDENT('dbo.Job', RESEED, 0);
+DBCC CHECKIDENT('dbo.Manufacturer', RESEED, 0);
+DBCC CHECKIDENT('dbo.Role', RESEED, 0);
 DBCC CHECKIDENT('dbo.Vendor', RESEED, 0);
